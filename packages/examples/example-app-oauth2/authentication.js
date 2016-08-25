7859fc60f4a75280c83c4a5557720de1221d1641
Brian Cooksey <bac2k@hotmail.com>
2016-08-25 17:47:21 -0500

    Break authentication section out of main app def and into separate file

---
+++ packages/examples/example-app-oauth2/authentication.js
@@ -0,0 +1,105 @@
+const baseURL = 'http://zapier.webscript.io/platform-example-app';
+
+const getAccessToken = (z, bundle) => {
+  const promise = z.request(`${baseURL}/access-token`, {
+    method: 'POST',
+    body: {
+      code: bundle.inputData.code,
+      client_id: bundle.environment.CLIENT_ID,
+      client_secret: bundle.environment.CLIENT_SECRET,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+      refresh_token: result.refresh_token
+    };
+  });
+};
+
+const refreshAccessToken = (z, bundle) => {
+  const promise = z.request(`${baseURL}/refresh-token`, {
+    method: 'POST',
+    body: {
+      refresh_token: bundle.authData.refresh_token,
+      client_id: bundle.environment.CLIENT_ID,
+      client_secret: bundle.environment.CLIENT_SECRET,
+      grant_type: 'refresh_token'
+    },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
+  // return it here to update the user's auth on Zapier.
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token
+    };
+  });
+};
+
+const testAuth = (z, bundle) => {
+  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
+  // every user will have access to, such as an account or profile endpoint like /me.
+  const promise = z.request({
+    url: `${baseURL}/auth-test`,
+  });
+
+  // This method can return any truthy value to indicate the credentials are valid.
+  // Raise an error to show
+  return promise.then((response) => {
+    if (response.status === 401) {
+      throw new Error('The access token you supplied is not valid');
+    }
+    return response;
+  });
+};
+
+module.exports = {
+  type: 'oauth2',
+  oauth2Config: {
+    // Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
+    // Zapier generates the state and redirect_uri, you are responsible for providing the rest.
+    // Note: can also be a function that returns a string
+    authorizeUrl: {
+      url: `${baseURL}/authorize`,
+      params: {
+        client_id: '{{bundle.environment.CLIENT_ID}}',
+        state: '{{bundle.inputData.state}}',
+        redirect_uri: '{{bundle.inputData.redirect_uri}}',
+        response_type: 'code'
+      }
+    },
+    // Step 2 of the OAuth flow; Exchange a code for an access token.
+    // This could also use the request shorthand.
+    getAccessToken: getAccessToken,
+    // (Optional) If the access token expires after a pre-defined amount of time, you can implement
+    // this method to tell Zapier how to refresh it.
+    refreshAccessToken: refreshAccessToken,
+    autoRefresh: true
+    // If there is a specific scope you want to limit your Zapier app to, you can define it here.
+    // Will get passed along to the authorizeUrl
+    // scope: 'read,write'
+  },
+  // The test method allows Zapier to verify that the access token is valid. We'll execute this
+  // method after the OAuth flow is complete to ensure everything is setup properly.
+  test: testAuth
+};
--- packages/examples/example-app-oauth2/index.js
+++ packages/examples/example-app-oauth2/index.js
@@ -3,8 +3,7 @@ Welcome to the Zapier OAuth2 example app!
 This app showcases the necessary methods needed to perform the OAuth2 flow.
 It also illustrates how to add the authorization header to later requests.
 */
-
-const baseURL = 'http://zapier.webscript.io/platform-example-app';
+const authentication = require('./authentication');
 
 const App = {
   // This is just shorthand to reference the installed dependencies you have. Zapier will
@@ -12,102 +11,7 @@ const App = {
   version: require('./package.json').version,
   platformVersion: require('./package.json').dependencies['@zapier/zapier-platform-core'],
 
-  authentication: {
-    type: 'oauth2',
-    oauth2Config: {
-      // Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
-      // Zapier generates the state and redirect_uri, you are responsible for providing the rest.
-      authorizeUrl: {
-        url: `${baseURL}/authorize`,
-        params: {
-          client_id: '{{bundle.environment.CLIENT_ID}}',
-          state: '{{bundle.inputData.state}}',
-          redirect_uri: '{{bundle.inputData.redirect_uri}}',
-          response_type: 'code'
-        }
-      },
-      // Step 2 of the OAuth flow; Exchange a code for an access token.
-      // This could also use the request shorthand.
-      getAccessToken: (z, bundle) => {
-        const promise = z.request(`${baseURL}/access-token`, {
-          method: 'POST',
-          body: {
-            code: bundle.inputData.code,
-            client_id: bundle.environment.CLIENT_ID,
-            client_secret: bundle.environment.CLIENT_SECRET,
-            grant_type: 'authorization_code'
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        });
-
-        // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
-        return promise.then((response) => {
-          if (response.status !== 200) {
-            throw new Error('Unable to fetch access token: ' + response.content);
-          }
-
-          const result = JSON.parse(response.content);
-          return {
-            access_token: result.access_token,
-            refresh_token: result.refresh_token
-          };
-        });
-      },
-      // (Optional) If the access token expires after a pre-defined amount of time, you can implement
-      // this method to tell Zapier how to refresh it.
-      refreshAccessToken: (z, bundle) => {
-        const promise = z.request(`${baseURL}/refresh-token`, {
-          method: 'POST',
-          body: {
-            refresh_token: bundle.authData.refresh_token,
-            client_id: bundle.environment.CLIENT_ID,
-            client_secret: bundle.environment.CLIENT_SECRET,
-            grant_type: 'refresh_token'
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        });
-
-        // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
-        // return it here to update the user's auth on Zapier.
-        return promise.then((response) => {
-          if (response.status !== 200) {
-            throw new Error('Unable to fetch access token: ' + response.content);
-          }
-
-          const result = JSON.parse(response.content);
-          return {
-            access_token: result.access_token
-          };
-        });
-      },
-      autoRefresh: true
-      // If there is a specific scope you want to limit your Zapier app to, you can define it here.
-      // Will get passed along to the authorizeUrl
-      // scope: 'read,write'
-    },
-    // The test method allows Zapier to verify that the access token is valid. We'll execute this
-    // method after the OAuth flow is complete to ensure everything is setup properly.
-    test: (z, bundle) => {
-      // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
-      // every user will have access to, such as an account or profile endpoint like /me.
-      const promise = z.request({
-        url: `${baseURL}/auth-test`,
-      });
-
-      // This method can return any truthy value to indicate the credentials are valid.
-      // Raise an error to show
-      return promise.then((response) => {
-        if (response.status === 401) {
-          throw new Error('The access token you supplied is not valid');
-        }
-        return response;
-      });
-    }
-  },
+  authentication: authentication,
 
   beforeRequest: [
     // To include the Authorization header on all outbound requests, simply define a function here.
