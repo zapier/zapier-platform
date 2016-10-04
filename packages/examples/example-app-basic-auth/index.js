de719d62f53d706e4922c7dfeb8dc73af969ea79
Brian Cooksey <bac2k@hotmail.com>
2016-10-04 17:35:13 -0500

    Rename writes -> creates

--- packages/examples/example-app-basic-auth/index.js
+++ packages/examples/example-app-basic-auth/index.js
@@ -31,8 +31,8 @@ const App = {
   searches: {
   },
 
-  // If you want your writes to show up, you better include it here!
-  writes: {
+  // If you want your creates to show up, you better include it here!
+  creates: {
   }
 };
 
