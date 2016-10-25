9319a4141169abcb489c3f11b513de56031efd61
Bryan Helmig <bryan@zapier.com>
2016-10-25 09:56:05 -0700

    drop extra comments

--- packages/examples/example-app-search/index.js
+++ packages/examples/example-app-search/index.js
@@ -1,12 +1,3 @@
-/*
-## Welcome to the Zapier search example app!
-
-This is a small example, all in one file. You can get a clone and start it
-from `zapier init --template=search`. You can of course break apart your
-example app, no reason to make it a single file. Also, we recommend taking
-a look at the automated tests you can do via `zapier test`!
-*/
-
 const search = require('./searches/recipe');
 
 // Now we can roll up all our behaviors in an App.
