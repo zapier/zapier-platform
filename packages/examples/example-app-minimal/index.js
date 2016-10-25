bf630529120c93131aaa00747dc53ac0707cea56
Bryan Helmig <bryan@zapier.com>
2016-10-25 09:55:47 -0700

    drop extra comments

--- packages/examples/example-app-minimal/index.js
+++ packages/examples/example-app-minimal/index.js
@@ -1,12 +1,3 @@
-/*
-## Welcome to the Zapier example app!
-
-This is a small example, all in one file. You can get a clone and start it
-from `zapier init --template=minimal`. You can of course break apart your
-example app, no reason to make it a single file. Also, we recommend taking
-a look at the automated tests you can do via `zapier test`!
-*/
-
 // We can roll up all our behaviors in an App.
 const App = {
   // This is just shorthand to reference the installed dependencies you have. Zapier will
