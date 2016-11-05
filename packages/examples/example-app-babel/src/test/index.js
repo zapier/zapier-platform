952a783ae4d0335741259348d9f396cdcf2d7cd2
Bryan Helmig <bryan@zapier.com>
2016-11-04 17:16:57 -0700

    add a mochaAsync helper

--- packages/examples/example-app-babel/src/test/index.js
+++ packages/examples/example-app-babel/src/test/index.js
@@ -4,12 +4,22 @@ import zapier from 'zapier-platform-core';
 
 const appTester = zapier.createAppTester(require('../index'));
 
+const mochaAsync = (fn) => {
+  return async (done) => {
+    try {
+      await fn();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  };
+};
+
 describe('My Test', () => {
 
-  it('should test the auth', async (done) => {
+  it('should test the auth', mochaAsync(async () => {
     const response = await appTester('authentication.test');
     should(response.status).eql(200);
-    done();
-  });
+  }));
 
 });
