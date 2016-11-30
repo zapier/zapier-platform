ddf0b33006d40807d3988ace4789374adde56eeb
Bryan Helmig <bryan@zapier.com>
2016-11-30 12:37:00 -0800

    prep for path resolving change

--- packages/examples/example-app-babel/src/test/index.js
+++ packages/examples/example-app-babel/src/test/index.js
@@ -2,7 +2,8 @@ import should from 'should';
 
 import zapier from 'zapier-platform-core';
 
-const appTester = zapier.createAppTester(require('../index'));
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
 
 const mochaAsync = (fn) => {
   return async (done) => {
@@ -18,7 +19,7 @@ const mochaAsync = (fn) => {
 describe('My Test', () => {
 
   it('should test the auth', mochaAsync(async () => {
-    const response = await appTester('authentication.test');
+    const response = await appTester(App.authentication.test);
     should(response.status).eql(200);
   }));
 
