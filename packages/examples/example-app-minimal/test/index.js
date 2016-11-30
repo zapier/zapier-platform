ff5272c09badba44b146d8afc665f1ed6eef8548
Bryan Helmig <bryan@zapier.com>
2016-11-30 11:35:43 -0800

    prep for path resolving change

--- packages/examples/example-app-minimal/test/index.js
+++ packages/examples/example-app-minimal/test/index.js
@@ -3,7 +3,8 @@ require('should');
 const zapier = require('zapier-platform-core');
 
 // Use this to make test calls into your app:
-const appTester = zapier.createAppTester(require('../index'));
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
 
 describe('My App', () => {
 
