eab92005786258ffe97d2b02751862b71276a55a
Bryan Helmig <bryan@zapier.com>
2016-11-30 12:36:35 -0800

    prep for path resolving change

--- packages/examples/example-app-middleware/test/triggers.js
+++ packages/examples/example-app-middleware/test/triggers.js
@@ -2,13 +2,14 @@ require('should');
 
 const zapier = require('zapier-platform-core');
 
-const appTester = zapier.createAppTester(require('../index'));
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
 
 describe('triggers', () => {
 
   describe('new recipe trigger', () => {
     it('should load recipes', (done) => {
-      appTester('triggers.recipe')
+      appTester(App.triggers.recipe.operation.perform)
         .then(results => {
           const firstRecipe = results[0];
           firstRecipe.name.should.eql('Smith Family Recipe');
