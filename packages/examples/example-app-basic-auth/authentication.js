33c4dd8f960e37d12047a2d7b9192990646572c6
Brian Cooksey <bac2k@hotmail.com>
2017-03-21 14:48:32 -0500

    Bump to version 1.0.2 of core
    Also fixed a couple eslint errors

--- packages/examples/example-app-basic-auth/authentication.js
+++ packages/examples/example-app-basic-auth/authentication.js
@@ -1,9 +1,9 @@
-const test = (z, bundle) => {
+const test = (z /*, bundle*/) => {
   // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
   // every user will have access to, such as an account or profile endpoint like /me.
   // In this example, we'll hit httpbin, which validates the Authorization Header against the arguments passed in the URL path
   const promise = z.request({
-    url: `http://httpbin.org/basic-auth/user/passwd`,
+    url: 'http://httpbin.org/basic-auth/user/passwd',
   });
 
   // This method can return any truthy value to indicate the credentials are valid.
