4a75851456ac3f97a0d286aa9dc0e6fd1271110a
Bruno Bernardino <bruno@bernardino.io>
2017-04-18 18:30:59 +0100

    Merge pull request #1 from zapier/feature-improvements
    Adding improvements, removing unnecessary dependencies.

--- packages/examples/example-app-create/creates/recipe.js
+++ packages/examples/example-app-create/creates/recipe.js
@@ -15,8 +15,9 @@ module.exports = {
   operation: {
     inputFields: [
       {key: 'name', required: true, type: 'string'},
-      {key: 'directions', required: true, type: 'text'},
-      {key: 'authorId', required: true, type: 'integer'}
+      {key: 'directions', required: true, type: 'text', helpText: 'Explain how should one make the recipe, step by step.'},
+      {key: 'authorId', required: true, type: 'integer', label: 'Author ID'},
+      {key: 'style', required: false, type: 'string', helpText: 'Explain what style of cuisine this is.'},
     ],
     perform: (z, bundle) => {
       const promise = z.request({
@@ -26,6 +27,7 @@ module.exports = {
           name: bundle.inputData.name,
           directions: bundle.inputData.directions,
           authorId: bundle.inputData.authorId,
+          style: bundle.inputData.style,
         }),
         headers: {
           'content-type': 'application/json'
@@ -34,5 +36,30 @@ module.exports = {
 
       return promise.then((response) => JSON.parse(response.content));
     }
-  }
+  },
+
+  // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
+  // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
+  // returned records, and have obviously dummy values that we can show to any user.
+  sample: {
+    id: 1,
+    createdAt: 1472069465,
+    name: 'Best Spagetti Ever',
+    authorId: 1,
+    directions: '1. Boil Noodles\n2.Serve with sauce',
+    style: 'italian',
+  },
+
+  // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
+  // field definitions. The result will be used to augment the sample.
+  // outputFields: () => { return []; }
+  // Alternatively, a static field definition should be provided, to specify labels for the fields
+  outputFields: [
+    {key: 'id', label: 'ID'},
+    {key: 'createdAt', label: 'Created At'},
+    {key: 'name', label: 'Name'},
+    {key: 'directions', label: 'Directions'},
+    {key: 'authorId', label: 'Author ID'},
+    {key: 'style', label: 'Style'},
+  ]
 };
