16909b477c0f828134a4e4ab35b2faa0381a5d73
Bruno Bernardino <bruno@bernardino.io>
2017-05-04 11:33:03 +0100

    Move sample and outputFields
    These were invalid. Caught in zapier/zapier-platform-cli#77

--- packages/examples/example-app-search/searches/recipe.js
+++ packages/examples/example-app-search/searches/recipe.js
@@ -35,31 +35,31 @@ module.exports = {
 
       return z.request(url, options)
         .then(response => JSON.parse(response.content));
-    }
-  },
+    },
+    
+    // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
+    // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
+    // returned records, and have obviously dummy values that we can show to any user.
+    sample: {
+      id: 1,
+      createdAt: 1472069465,
+      name: 'Best Spagetti Ever',
+      authorId: 1,
+      directions: '1. Boil Noodles\n2.Serve with sauce',
+      style: 'italian'
+    },
 
-  // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
-  // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
-  // returned records, and have obviously dummy values that we can show to any user.
-  sample: {
-    id: 1,
-    createdAt: 1472069465,
-    name: 'Best Spagetti Ever',
-    authorId: 1,
-    directions: '1. Boil Noodles\n2.Serve with sauce',
-    style: 'italian',
-  },
-
-  // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
-  // field definitions. The result will be used to augment the sample.
-  // outputFields: () => { return []; }
-  // Alternatively, a static field definition should be provided, to specify labels for the fields
-  outputFields: [
-    {key: 'id', label: 'ID'},
-    {key: 'createdAt', label: 'Created At'},
-    {key: 'name', label: 'Name'},
-    {key: 'directions', label: 'Directions'},
-    {key: 'authorId', label: 'Author ID'},
-    {key: 'style', label: 'Style'},
-  ]
+    // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
+    // field definitions. The result will be used to augment the sample.
+    // outputFields: () => { return []; }
+    // Alternatively, a static field definition should be provided, to specify labels for the fields
+    outputFields: [
+      {key: 'id', label: 'ID'},
+      {key: 'createdAt', label: 'Created At'},
+      {key: 'name', label: 'Name'},
+      {key: 'directions', label: 'Directions'},
+      {key: 'authorId', label: 'Author ID'},
+      {key: 'style', label: 'Style'}
+    ]
+  }
 };
