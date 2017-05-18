875eca88b5c36ef7dd195f0c93e498a96aa82a2c
David Brownman <beamneocube@gmail.com>
2017-05-18 16:59:12 -0700

    add test

--- packages/zapier-platform-core/test/checks.js
+++ packages/zapier-platform-core/test/checks.js
@@ -2,14 +2,19 @@
 
 require('should');
 
-const createIsSingle = require('../src/checks/create-is-single');
+const checks = require('../src/checks');
 
 describe('checks', () => {
   it('createIsSingle sees create return values with multiples', () => {
-    createIsSingle.run('some.method', []).length.should.eql(0);
-    createIsSingle.run('some.method', [{}]).length.should.eql(0);
-    createIsSingle.run('some.method', {}).length.should.eql(0);
-    // the only error in this set:
-    createIsSingle.run('some.method', [{}, {}]).length.should.eql(1);
+    checks.createIsSingle.run('some.method', []).length.should.eql(0);
+    checks.createIsSingle.run('some.method', [{}]).length.should.eql(0);
+    checks.createIsSingle.run('some.method', {}).length.should.eql(0);
+
+    checks.createIsSingle.run('some.method', [{}, {}]).length.should.eql(1);
+  });
+
+  it('searchIsArray should error for objects', () => {
+    checks.searchIsArray.run('some.method', [{}, {}]).length.should.eql(0);
+    checks.searchIsArray.run('some.method', {}).length.should.eql(1);
   });
 });
