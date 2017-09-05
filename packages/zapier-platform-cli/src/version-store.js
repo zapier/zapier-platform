bb7055ab206c36915859e062c299073bafcf8ebf
Chang-Hung Liang <eliang.cs@gmail.com>
2017-09-05 17:46:57 +0800

    Add node/npm deps for new core version

--- packages/zapier-platform-cli/src/version-store.js
+++ packages/zapier-platform-cli/src/version-store.js
@@ -3,5 +3,6 @@
 module.exports = [
   {nodeVersion: '4.3.2', npmVersion: '>=2.14.12'},
   {nodeVersion: '4.3.2', npmVersion: '>=2.14.12'},
+  {nodeVersion: '6.10.2', npmVersion: '>=3.10.10'},
   {nodeVersion: '6.10.2', npmVersion: '>=3.10.10'}
 ];
