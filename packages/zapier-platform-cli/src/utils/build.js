4f19e72e3315cb7670010a686bfda5a7415edf4b
Bruno Bernardino <bruno@bernardino.io>
2017-10-31 10:34:06 +0000

    Merge pull request #169 from zapier/feature-allow-deasync-for-interpreter
     Allow linux x64 node-6 binary in builds.

--- packages/zapier-platform-cli/src/utils/build.js
+++ packages/zapier-platform-cli/src/utils/build.js
@@ -114,6 +114,7 @@ const forceIncludeDumbPath = (filePath/*, smartPaths*/) => {
   // we include smartPaths just incase you want to check the inclusion of some library
   return (
     (filePath.endsWith('package.json') || filePath.endsWith('definition.json'))
+    || filePath.endsWith('/bin/linux-x64-node-6/deasync.node') // Special, for zapier-platform-legacy-scripting-runner
     || filePath.match(path.sep === '\\' ? /aws-sdk\\apis\\.*\.json/ : /aws-sdk\/apis\/.*\.json/)
     || (global.argOpts['include-js-map'] && filePath.endsWith('.js.map'))
   );
