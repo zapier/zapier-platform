697bc0074fb0bc7851a40a6d038d87cd01fff9f0
Chang-Hung Liang <eliang.cs@gmail.com>
2018-03-15 14:51:17 +0800

    Merge pull request #1 from zapier/keep-headers
    Keep whatever headers passed into runEvent

--- packages/examples/zapier-platform-legacy-scripting-runner/bundle.js
+++ packages/examples/zapier-platform-legacy-scripting-runner/bundle.js
@@ -61,6 +61,9 @@ const addAuthData = (event, bundle, convertedBundle) => {
     convertedBundle.request.auth = [username, password];
   }
 
+  const headers = _.get(bundle, ['request', 'headers'], {});
+  _.extend(convertedBundle.request.headers, headers);
+
   // OAuth2 specific
   if (event.name.startsWith('auth.oauth2')) {
     convertedBundle.oauth_data = {
