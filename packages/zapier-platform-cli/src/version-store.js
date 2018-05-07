99320eba99b5f410267faa8cdf61cb7d737f3b10
Chang-Hung Liang <chang-hung.liang@zapier.com>
2018-05-07 17:04:28 +0800

    Add 6.x to version-store

--- packages/zapier-platform-cli/src/version-store.js
+++ packages/zapier-platform-cli/src/version-store.js
@@ -1,5 +1,5 @@
 // Index of each item is equivalent to the major of zapier-platform-core version.
-// The next item will be for zapier-platform-core version of major 6.
+// The next item will be for zapier-platform-core version of major 7.
 
 // node version listed here: https://docs.aws.amazon.com/lambda/latest/dg/current-supported-versions.html
 module.exports = [
@@ -8,5 +8,6 @@ module.exports = [
   { nodeVersion: '6.10.2', npmVersion: '>=3.10.10' }, // 2.x
   { nodeVersion: '6.10.2', npmVersion: '>=3.10.10' }, // 3.x
   { nodeVersion: '6.10.2', npmVersion: '>=3.10.10' }, // 4.x
-  { nodeVersion: '6.10.3', npmVersion: '>=3.10.10' } // 5.x // patch version change on AWS
+  { nodeVersion: '6.10.3', npmVersion: '>=3.10.10' }, // 5.x // patch version change on AWS
+  { nodeVersion: '6.10.3', npmVersion: '>=3.10.10' } // 6.x
 ];
