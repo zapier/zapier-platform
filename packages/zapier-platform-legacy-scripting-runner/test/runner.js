0a44d121eadd51c3c3ab7b4019aabb938e6da1b6
Chang-Hung Liang <eliang.cs@gmail.com>
2018-05-16 17:55:00 +0800

    Merge pull request #3 from zapier/new-run-method
    Add legacyScriptingRunner.run() method

--- packages/zapier-platform-legacy-scripting-runner/test/runner.js
+++ packages/zapier-platform-legacy-scripting-runner/test/runner.js
@@ -1,87 +1,88 @@
-require('should');
+const should = require('should');
 
 const scriptingRunner = require('../index');
 
 describe('scriptingRunner', () => {
-
   const defaultBundle = {
     _legacyUrl: 'https://zapier.com',
     inputData: {
-      user: 'Zapier',
+      user: 'Zapier'
     },
     authData: {
-      apiKey: 'Zapier-API-Key',
+      apiKey: 'Zapier-API-Key'
     },
     meta: {
       frontend: false,
-      prefill: false,
-    },
+      prefill: false
+    }
   };
 
   const z = {
-    request: () => {},
+    request: () => {}
   };
 
-  it('should return nothing if there is no scripting', (done) => {
+  it('should return nothing if there is no scripting', done => {
     const event = {
       name: 'trigger.poll',
       key: 'trigger',
       response: {
         status: 200,
-        content: '[{"id": 1, "name": "Zapier"}]',
-      },
+        content: '[{"id": 1, "name": "Zapier"}]'
+      }
     };
     const bundle = defaultBundle;
     const Zap = {};
 
     const legacyScriptingRunner = scriptingRunner(Zap);
 
-    legacyScriptingRunner.runEvent(event, z, bundle)
-      .then((result) => {
+    legacyScriptingRunner
+      .runEvent(event, z, bundle)
+      .then(result => {
         should(result).eql(undefined);
         done();
       })
       .catch(done);
   });
 
-  it('should return nothing if there is no event', (done) => {
+  it('should return nothing if there is no event', done => {
     const event = {};
     const bundle = defaultBundle;
     const Zap = {
-      trigger_poll: () => (true),
+      trigger_poll: () => true
     };
 
     const legacyScriptingRunner = scriptingRunner(Zap);
 
-    legacyScriptingRunner.runEvent(event, z, bundle)
-      .then((result) => {
+    legacyScriptingRunner
+      .runEvent(event, z, bundle)
+      .then(result => {
         should(result).eql(undefined);
         done();
       })
       .catch(done);
   });
 
-  it('should return nothing if there is no event.name', (done) => {
+  it('should return nothing if there is no event.name', done => {
     const event = {
       key: 'trigger',
       response: {
         status: 200,
-        content: '[{"id": 1, "name": "Zapier"}]',
-      },
+        content: '[{"id": 1, "name": "Zapier"}]'
+      }
     };
     const bundle = defaultBundle;
     const Zap = {
-      trigger_poll: () => (true),
+      trigger_poll: () => true
     };
 
     const legacyScriptingRunner = scriptingRunner(Zap);
 
-    legacyScriptingRunner.runEvent(event, z, bundle)
-      .then((result) => {
+    legacyScriptingRunner
+      .runEvent(event, z, bundle)
+      .then(result => {
         should(result).eql(undefined);
         done();
       })
       .catch(done);
   });
-
 });
