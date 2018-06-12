48305b7ce9e18155d9b000a105d069c374524465
Chang-Hung Liang <eliang.cs@gmail.com>
2018-06-12 22:19:13 +0800

    Merge pull request #6 from zapier/session-auth-multi-keys
    Include entire session data, not just single key

--- packages/zapier-platform-legacy-scripting-runner/index.js
+++ packages/zapier-platform-legacy-scripting-runner/index.js
@@ -131,44 +131,6 @@ const legacyScriptingRunner = (Zap, zobj, app) => {
       return undefined;
     });
 
-  const runSessionAuth = bundle => {
-    return runEvent({ name: 'auth.session' }, zobj, bundle).then(
-      sessionData => {
-        // WB apps in scripting's get_session_info() allowed you to return any
-        // object and that would be added to the authData, but CLI apps require
-        // you to specifically define those. That means that if you return more
-        // than one key from your scripting's get_session_info(), Zapier staff
-        // will need to manually set sessionConfig.legacyProperties.sessionKeyKey
-        // for the dev to select a value from the object returned from
-        // get_session_info().
-
-        let sessionKey;
-        const keys = Object.keys(sessionData);
-        if (_.isEmpty(keys)) {
-          throw new Error('Empty result from get_session_info()');
-        } else if (keys.length === 1) {
-          sessionKey = sessionData[keys[0]];
-        } else {
-          const sessionKeyKey = _.get(
-            app,
-            'authentication.sessionConfig.legacyProperties.sessionKeyKey'
-          );
-          if (!sessionKeyKey) {
-            throw new Error(
-              'Must configure sessionKeyKey if get_session_info() returns more than one item'
-            );
-          }
-          if (keys.indexOf(sessionKeyKey) < 0) {
-            throw new Error(`sessionKeyKey must be in (${keys})`);
-          }
-          sessionKey = sessionData[sessionKeyKey];
-        }
-
-        return { sessionKey };
-      }
-    );
-  };
-
   const runTrigger = (bundle, key) => {
     let promise = null;
     const funcs = [];
@@ -224,7 +186,9 @@ const legacyScriptingRunner = (Zap, zobj, app) => {
   const run = (bundle, typeOf, key) => {
     switch (typeOf) {
       case 'auth.session':
-        return runSessionAuth(bundle);
+        return runEvent({name: 'auth.session'}, zobj, bundle);
+      case 'auth.connectionLabel':
+        return runEvent({name: 'auth.connectionLabel'}, zobj, bundle);
       case 'trigger':
         return runTrigger(bundle, key);
     }
