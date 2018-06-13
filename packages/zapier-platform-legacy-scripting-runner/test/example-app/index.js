43b8beab5ca76e5da77e7471857a22485838c485
Chang-Hung Liang <eliang.cs@gmail.com>
2018-06-13 16:53:38 +0800

    Merge pull request #7 from zapier/integration-test
    Move legacy scripting tests out of core

---
+++ packages/zapier-platform-legacy-scripting-runner/test/example-app/index.js
@@ -0,0 +1,134 @@
+const legacyScriptingSource = `
+    var Zap = {
+      get_session_info: function(bundle) {
+        var encodedCredentials = btoa(bundle.auth_fields.username + ':' + bundle.auth_fields.password);
+        var response = z.request({
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization': 'Basic ' + encodedCredentials
+          },
+          url: 'https://auth-json-server.zapier.ninja/me'
+        });
+
+        if (response.status_code !== 200) {
+          throw new HaltedException('Auth failed: ' + response.content);
+        }
+
+        // Endpoint /me doesn't really give us an API key. We're just
+        // simulating username/password login in exchange of an API key here.
+        return {
+          key1: 'sec',
+          key2: 'ret'
+        };
+      },
+
+      contact_full_poll: function(bundle) {
+        var response = z.request({
+          url: 'https://auth-json-server.zapier.ninja/users',
+          params: { api_key: 'secret' }
+        });
+        var contacts = z.JSON.parse(response.content);
+        contacts[0].name = 'Patched by KEY_poll!';
+        return contacts;
+      },
+
+      contact_pre_pre_poll: function(bundle) {
+        bundle.request = {
+          url: 'https://auth-json-server.zapier.ninja/users',
+          params: { api_key: 'secret', id: 3 }
+        };
+        return bundle.request;
+      },
+
+      contact_post_post_poll: function(bundle) {
+        var contacts = z.JSON.parse(bundle.response.content);
+        contacts[0].name = 'Patched by KEY_post_poll!';
+        return contacts;
+      },
+
+      contact_pre_post_pre_poll: function(bundle) {
+        bundle.request = {
+          url: 'https://auth-json-server.zapier.ninja/users',
+          params: { api_key: 'secret', id: 4 }
+        };
+        return bundle.request;
+      },
+
+      contact_pre_post_post_poll: function(bundle) {
+        var contacts = z.JSON.parse(bundle.response.content);
+        contacts[0].name = 'Patched by KEY_pre_poll & KEY_post_poll!';
+        return contacts;
+      },
+    };
+`;
+
+const ContactTrigger_full = {
+  key: 'contact_full',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Full Scripting'
+  },
+  operation: {
+    perform: {
+      source: "return z.legacyScripting.run(bundle, 'trigger', 'contact_full');"
+    }
+  }
+};
+
+const ContactTrigger_pre = {
+  key: 'contact_pre',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Pre Scripting'
+  },
+  operation: {
+    perform: {
+      source: "return z.legacyScripting.run(bundle, 'trigger', 'contact_pre');"
+    }
+  }
+};
+
+const ContactTrigger_post = {
+  key: 'contact_post',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Post Scripting'
+  },
+  operation: {
+    legacyProperties: {
+      url: 'https://auth-json-server.zapier.ninja/users?api_key=secret'
+    },
+    perform: {
+      source: "return z.legacyScripting.run(bundle, 'trigger', 'contact_post');"
+    }
+  }
+};
+
+const ContactTrigger_pre_post = {
+  key: 'contact_pre_post',
+  noun: 'Contact',
+  display: {
+    label: 'New Contact with Pre & Post Scripting'
+  },
+  operation: {
+    perform: {
+      source:
+        "return z.legacyScripting.run(bundle, 'trigger', 'contact_pre_post');"
+    }
+  }
+};
+
+const App = {
+  title: 'Example App',
+  triggers: {
+    [ContactTrigger_full.key]: ContactTrigger_full,
+    [ContactTrigger_pre.key]: ContactTrigger_pre,
+    [ContactTrigger_post.key]: ContactTrigger_post,
+    [ContactTrigger_pre_post.key]: ContactTrigger_pre_post
+  },
+  legacyScriptingSource
+};
+
+module.exports = App;
