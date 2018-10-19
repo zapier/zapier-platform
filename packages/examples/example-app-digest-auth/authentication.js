5aa131320a5867e146880ad853e198e9ea4aaf57
Chang-Hung Liang <eliang.cs@gmail.com>
2018-10-19 22:27:49 +0800

    Initial commit

---
+++ packages/examples/example-app-digest-auth/authentication.js
@@ -0,0 +1,31 @@
+const test = (z /*, bundle*/) => {
+  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
+  // every user will have access to, such as an account or profile endpoint like /me.
+  // In this example, we'll hit httpbin, which validates the Authorization Header against the arguments passed in the URL path
+
+  // This method can return any truthy value to indicate the credentials are valid.
+  // Raise an error to show
+  return z
+    .request({
+      url: 'https://zapier-httpbin.herokuapp.com/digest-auth/auth/myuser/mypass'
+      // url: 'https://httpbin.org/digest-auth/auth/myuser/mypass'
+    })
+    .then(response => {
+      if (response.status === 401) {
+        throw new Error(
+          'The username and/or password you supplied is incorrect'
+        );
+      }
+      return response;
+    });
+};
+
+module.exports = {
+  type: 'digest',
+
+  // The test method allows Zapier to verify that the credentials a user provides are valid. We'll execute this
+  // method whenver a user connects their account for the first time.
+  test: test,
+  // assuming "username" is a key returned from the test
+  connectionLabel: '{{username}}'
+};
---
+++ packages/examples/example-app-digest-auth/index.js
@@ -0,0 +1,35 @@
+
+const authentication = require('./authentication');
+
+const App = {
+  // This is just shorthand to reference the installed dependencies you have. Zapier will
+  // need to know these before we can upload
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  authentication: authentication,
+
+  beforeRequest: [
+  ],
+
+  afterResponse: [
+  ],
+
+  resources: {
+  },
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: {
+  },
+
+  // If you want your searches to show up, you better include it here!
+  searches: {
+  },
+
+  // If you want your creates to show up, you better include it here!
+  creates: {
+  }
+};
+
+// Finally, export the app.
+module.exports = App;
