065f3c56e3077bce8a17ab72bb56ab1ceb902686
Chang-Hung Liang <eliang.cs@gmail.com>
2018-11-07 16:39:54 +0800

    Initial commit

---
+++ example-apps/oauth1-tumblr/authentication.js
@@ -0,0 +1,78 @@
+const _ = require('lodash');
+
+const REQUEST_TOKEN_URL = 'https://www.tumblr.com/oauth/request_token';
+const ACCESS_TOKEN_URL = 'https://www.tumblr.com/oauth/access_token';
+const AUTHORIZE_URL = 'https://www.tumblr.com/oauth/authorize';
+
+const getConnectionLabel = (z, bundle) => {
+  try {
+    return bundle.inputData.response.user.name;
+  } catch (err) {
+    return '';
+  }
+};
+
+const config = {
+  type: 'oauth1',
+  oauth1Config: {
+    getRequestToken: {
+      url: REQUEST_TOKEN_URL,
+      method: 'POST',
+      auth: {
+        oauth_consumer_key: '{{process.env.CLIENT_ID}}',
+        oauth_consumer_secret: '{{process.env.CLIENT_SECRET}}',
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_callback: '{{bundle.inputData.redirect_uri}}'
+      }
+    },
+    authorizeUrl: {
+      url: AUTHORIZE_URL,
+      params: {
+        oauth_token: '{{bundle.inputData.oauth_token}}',
+        name: 'Zapier OAuth1 Test'
+      }
+    },
+    getAccessToken: {
+      url: ACCESS_TOKEN_URL,
+      method: 'POST',
+      auth: {
+        oauth_consumer_key: '{{process.env.CLIENT_ID}}',
+        oauth_consumer_secret: '{{process.env.CLIENT_SECRET}}',
+        oauth_token: '{{bundle.inputData.oauth_token}}',
+        oauth_token_secret: '{{bundle.inputData.oauth_token_secret}}',
+        oauth_verifier: '{{bundle.inputData.oauth_verifier}}'
+      }
+    }
+  },
+  test: {
+    url: 'https://api.tumblr.com/v2/user/info'
+  },
+  connectionLabel: getConnectionLabel
+};
+
+// A middleware that includes the Authorization header on all outbound requests. It runs
+// runs before each request is sent out, allowing you to make tweaks to the request in a
+// centralized spot.
+const includeAccessToken = (req, z, bundle) => {
+  if (
+    bundle.authData &&
+    bundle.authData.oauth_token &&
+    bundle.authData.oauth_token_secret
+  ) {
+    // Just put your OAuth1 credentials in req.auth, Zapier will sign the request for
+    // you.
+    req.auth = req.auth || {};
+    _.defaults(req.auth, {
+      oauth_consumer_key: process.env.CLIENT_ID,
+      oauth_consumer_secret: process.env.CLIENT_SECRET,
+      oauth_token: bundle.authData.oauth_token,
+      oauth_token_secret: bundle.authData.oauth_token_secret
+    });
+  }
+  return req;
+};
+
+module.exports = {
+  config,
+  includeAccessToken
+};
---
+++ example-apps/oauth1-tumblr/triggers/like.js
@@ -0,0 +1,35 @@
+const perform = async (z, bundle) => {
+  const url = 'https://api.tumblr.com/v2/user/likes';
+  const response = await z.request(url);
+  const result = z.JSON.parse(response.content);
+  return result.response.liked_posts || [];
+};
+
+module.exports = {
+  key: 'like',
+  noun: 'Like',
+
+  display: {
+    label: 'New Like',
+    description: 'Triggers when you like a post.'
+  },
+
+  operation: {
+    perform,
+    sample: {
+      blog_name: 'citriccomics',
+      id: 3507845453,
+      post_url: 'http://citriccomics.tumblr.com/post/3507845453',
+      type: 'text',
+      date: '2011-02-25 20:27:00 GMT',
+      timestamp: 1298665620,
+      state: 'published',
+      format: 'html',
+      reblog_key: 'b0baQtsl',
+      tags: ['tumblrize', 'milky dog', 'mini comic'],
+      note_count: 14,
+      title: 'Milky Dog',
+      body: '<p>Example body.</p>'
+    }
+  }
+};
