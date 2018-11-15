0dc69f99ac658937f98d34a4cfd6c03856369314
Chang-Hung Liang <eliang.cs@gmail.com>
2018-11-15 17:41:26 +0800

    Make getAccessToken and getRequestToken a function

--- example-apps/oauth1-twitter/authentication.js
+++ example-apps/oauth1-twitter/authentication.js
@@ -1,38 +1,56 @@
+const querystring = require('querystring');
+
 const _ = require('lodash');
 
 const REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token';
 const AUTHORIZE_URL = 'https://api.twitter.com/oauth/authorize';
 const ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token';
 
+const getRequestToken = async (z, bundle) => {
+  const response = await z.request({
+    url: REQUEST_TOKEN_URL,
+    method: 'POST',
+    auth: {
+      oauth_consumer_key: process.env.CLIENT_ID,
+      oauth_consumer_secret: process.env.CLIENT_SECRET,
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_callback: bundle.inputData.redirect_uri,
+      oauth_version: '1.0' // Twitter says this should be 1.0
+    }
+  });
+  response.throwForStatus();
+  return querystring.parse(response.content);
+};
+
+const getAccessToken = async (z, bundle) => {
+  const response = await z.request({
+    url: ACCESS_TOKEN_URL,
+    method: 'POST',
+    auth: {
+      oauth_consumer_key: process.env.CLIENT_ID,
+      oauth_consumer_secret: process.env.CLIENT_SECRET,
+      oauth_token: bundle.inputData.oauth_token,
+      oauth_token_secret: bundle.inputData.oauth_token_secret,
+      oauth_verifier: bundle.inputData.oauth_verifier
+    }
+  });
+  response.throwForStatus();
+  return querystring.parse(response.content);
+};
+
 const config = {
   type: 'oauth1',
   oauth1Config: {
-    getRequestToken: {
-      url: REQUEST_TOKEN_URL,
-      method: 'POST',
-      auth: {
-        oauth_consumer_key: '{{process.env.CLIENT_ID}}',
-        oauth_consumer_secret: '{{process.env.CLIENT_SECRET}}',
-        oauth_signature_method: 'HMAC-SHA1',
-        oauth_callback: '{{bundle.inputData.redirect_uri}}',
-        oauth_version: '1.0'  // must be 1.0 for Twitter
-      }
-    },
+    // We have to define getRequestToken and getAccessToken functions to explicitly
+    // parse the response like it has a form body here, since Twitter responds
+    // 'text/html' for the Content-Type header
+    getRequestToken,
+    getAccessToken,
+
     authorizeUrl: {
       url: AUTHORIZE_URL,
       params: {
         oauth_token: '{{bundle.inputData.oauth_token}}'
-      }
-    },
-    getAccessToken: {
-      url: ACCESS_TOKEN_URL,
-      method: 'POST',
-      auth: {
-        oauth_consumer_key: '{{process.env.CLIENT_ID}}',
-        oauth_consumer_secret: '{{process.env.CLIENT_SECRET}}',
-        oauth_token: '{{bundle.inputData.oauth_token}}',
-        oauth_token_secret: '{{bundle.inputData.oauth_token_secret}}',
-        oauth_verifier: '{{bundle.inputData.oauth_verifier}}'
       }
     }
   },
