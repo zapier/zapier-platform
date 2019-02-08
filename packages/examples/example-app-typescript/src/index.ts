fb11a0792d0260ec4bda7117f5eaa67deb469187
David Brownman <beamneocube@gmail.com>
2019-02-08 14:05:13 -0500

    remove extra line

--- packages/examples/example-app-typescript/src/index.ts
+++ packages/examples/example-app-typescript/src/index.ts
@@ -3,8 +3,6 @@ import Recipe from "./resources/recipe";
 const { version } = require("../package.json");
 import { version as platformVersion } from "zapier-platform-core";
 
-process.version;
-
 const App = {
   version,
   platformVersion,
