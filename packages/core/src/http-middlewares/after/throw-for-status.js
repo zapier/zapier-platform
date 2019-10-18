c737413c1ec57b0dc53837e48298b3e490a49fa0
Chang-Hung Liang <eliang.cs@gmail.com>
2019-10-18 14:56:01 +0800

    Strip querystring from error message

--- packages/core/src/http-middlewares/after/throw-for-status.js
+++ packages/core/src/http-middlewares/after/throw-for-status.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const { stripQueryFromURL } = require('../../tools/http');
+
 const throwForStatus = resp => {
   if (resp.status > 300) {
-    const message = `Got ${resp.status} calling ${resp.request.method} ${
-      resp.request.url
-    }, expected 2xx.`;
+    const cleanURL = stripQueryFromURL(resp.request.url);
+    const message = `Got ${resp.status} calling ${resp.request.method} ${cleanURL}, expected 2xx.`;
     throw new Error(message);
   }
 
