fab54e5f268663f2892f5642f712ea1bfe74545f
Chris Johns <chris@ter0.net>
2020-03-04 12:59:16 +0000

    Add $.extend shim

--- packages/legacy-scripting-runner/test/integration-test.js
+++ packages/legacy-scripting-runner/test/integration-test.js
@@ -728,6 +728,7 @@ describe('Integration Test', () => {
         firstContact.isPlainObject.should.be.false();
         should.equal(firstContact.trimmed, 'hello world');
         should.equal(firstContact.type, 'array');
+        should.equal(firstContact.extend, { extended: true });
 
         const secondContact = output.results[1];
         should.equal(firstContact.anotherId, 1000);
