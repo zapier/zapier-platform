4b68e1280b85a955c8c4e6fd5683342da8ba4ded
David Brownman <beamneocube@gmail.com>
2020-04-27 20:52:14 -0700

    generate working basic auth

--- packages/cli/src/utils/init.js
+++ packages/cli/src/utils/init.js
@@ -4,6 +4,8 @@ const tmp = require('tmp');
 const { startSpinner, endSpinner } = require('./display');
 const { copyDir, removeDir, ensureDir } = require('./files');
 
+const { inspect } = require('util');
+
 const initApp = async (path, createFunc) => {
   const appDir = resolve(path);
   const tempAppDir = tmp.tmpNameSync();
@@ -28,6 +30,70 @@ const initApp = async (path, createFunc) => {
   await removeDir(tempAppDir);
 };
 
+const testAuth = authType => {
+  // todo: oauth1 trello url?
+  const testUrl =
+    authType === 'digest'
+      ? 'https://httpbin.org/digest-auth/auth/myuser/mypass'
+      : 'https://auth-json-server.zapier-staging.com/me';
+
+  return `
+      const test = (z, bundle) => {
+        return z.request({
+          url: '${testUrl}',
+          ${authType === 'oauth2' ? inspect({}) : ''}
+        })
+      }
+      `.trim();
+};
+
+/**
+ * given an authType (such as `basic` or `oauth2`, return a string of valid JS code that be saved into an `authentication.js` file
+ */
+const createAuthFile = authType => {
+  // TODO: call prettier on this
+
+  const suppliedItem =
+    {
+      custom: 'API Key',
+      oauth2: 'access token'
+    }[authType] || 'username and/or password';
+
+  const output = `
+    ${testAuth(authType)}
+
+    const handleBadResponses = (response, z, bundle) => {
+      if (response.status === 401) {
+        throw new z.errors.Error(
+          // This message is surfaced to the user
+          'The ${suppliedItem} you supplied is incorrect',
+          'AuthenticationError',
+          response.status
+        );
+      }
+      return response
+    }
+
+    module.exports = {
+      config: {
+        type: '${authType}',
+
+        // The test method allows Zapier to verify that the credentials a user provides are valid.
+        // We'll execute this method whenver a user connects their account for the first time.
+        test,
+
+        // this can be a "{{string}}" or a function
+        connectionLabel: '{{bundle.inputData.username}}'
+      },
+
+      befores: [],
+      afters: [ handleBadResponses ]
+    }
+  `.trim();
+  return output;
+};
+
 module.exports = {
-  initApp
+  initApp,
+  createAuthFile
 };
