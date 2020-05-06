3fda085d80020b8fc1ecfb81c631483f714453d5
Fokke Zandbergen <mail@fokkezb.nl>
2020-05-06 13:44:54 +0200

    Update packages/schema/lib/schemas/RequestSchema.js
    Co-authored-by: Chang-Hung Liang <chang-hung.liang@zapier.com>

--- packages/schema/lib/schemas/RequestSchema.js
+++ packages/schema/lib/schemas/RequestSchema.js
@@ -81,7 +81,7 @@ module.exports = makeSchema(
           'A function to customize how to serialize a value for curlies `{{var}}` in the request object. By default, when this is unspecified, the request client only replaces curlies where variables are strings, and would throw an error for non-strings. The function should accepts a single argument as the value to be serialized and return the string representation of the argument.',
         $ref: FunctionSchema.id
       },
-      skipThrowRequest: {
+      skipThrowForStatus: {
         description:
           'Should throwForStatus be calld before returning the response?',
         type: 'boolean',
