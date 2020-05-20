63ea2d223aef740e161467e15e5410c3debff12a
Chang-Hung Liang <chang-hung.liang@zapier.com>
2020-05-20 17:10:48 +0800

    fix(cli): fix npm registry dependency in tests

--- packages/cli/src/tests/_helpers.js
+++ packages/cli/src/tests/_helpers.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process');
-const { realpathSync } = require('fs');
+const { readFileSync, realpathSync, writeFileSync, unlinkSync } = require('fs');
 const { tmpdir } = require('os');
-const { join } = require('path');
+const { join, resolve } = require('path');
 const { randomBytes } = require('crypto');
 
 const runCommand = (cmd, args, opts = {}) => {
@@ -20,4 +20,62 @@ const randomStr = (length = 4) => randomBytes(length).toString('hex');
 const getNewTempDirPath = () =>
   join(realpathSync(tmpdir()), `zapier-${randomStr()}`);
 
-module.exports = { runCommand, getNewTempDirPath, randomStr };
+const getLastNonEmptyString = (arr) => {
+  let result = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const str = arr[i].trim();
+    if (str) {
+      result = str;
+      break;
+    }
+  }
+  return result;
+};
+
+const npmPackSchema = async () => {
+  const schemaDir = resolve(__dirname, '../../../../packages/schema');
+  const stdout = runCommand('npm', ['pack'], { cwd: schemaDir });
+  const filename = getLastNonEmptyString(stdout.split('\n'));
+  const filePath = join(schemaDir, filename);
+  return {
+    path: filePath,
+    cleanup: () => {
+      unlinkSync(filePath);
+    },
+  };
+};
+
+// TODO: Fix duplicate code with packages/core/smoke-test.js
+const npmPackCore = async () => {
+  const { cleanup: cleanupSchema, path: schemaPath } = await npmPackSchema();
+
+  // Patch core's package.json to use schema from local
+  const coreDir = resolve(__dirname, '../../../../packages/core');
+  const corePackageJsonPath = join(coreDir, 'package.json');
+  const originalPackageJsonText = readFileSync(corePackageJsonPath, {
+    encoding: 'utf8',
+  });
+  const corePackageJson = JSON.parse(originalPackageJsonText);
+  corePackageJson.dependencies['zapier-platform-schema'] = `file:${schemaPath}`;
+  writeFileSync(corePackageJsonPath, JSON.stringify(corePackageJson));
+
+  let stdout;
+  try {
+    stdout = runCommand('npm', ['pack'], { cwd: coreDir });
+  } finally {
+    writeFileSync(corePackageJsonPath, originalPackageJsonText);
+  }
+
+  const filename = getLastNonEmptyString(stdout.split('\n'));
+  const filePath = join(coreDir, filename);
+
+  return {
+    path: filePath,
+    cleanup: () => {
+      cleanupSchema();
+      unlinkSync(filePath);
+    },
+  };
+};
+
+module.exports = { runCommand, getNewTempDirPath, randomStr, npmPackCore };
