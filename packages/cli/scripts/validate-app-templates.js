fcc95ba9589cfbaf4b3063ab9e55a32075546ffa
Dani Weisz <danielle.weisz@zapier.com>
2020-08-21 13:20:09 -0700

    Remove more references to `npm run zapier-build` from babel example

--- packages/cli/scripts/validate-app-templates.js
+++ packages/cli/scripts/validate-app-templates.js
@@ -13,7 +13,6 @@ const appTemplates = require('../src/app-templates');
 const validateAppTemplate = (template, rootTmpDir) => {
   // const appDir = path.resolve(rootTmpDir, template);
   const zapierCmd = path.resolve(__dirname, '../src/bin/run');
-  const extraCmd = template === 'babel' ? ' && npm run zapier-build' : '';
 
   const logFile = path.resolve(__dirname, '..', `${template}.log`);
   const logStream = fse.createWriteStream(logFile);
@@ -25,8 +24,8 @@ const validateAppTemplate = (template, rootTmpDir) => {
     .ensureFile(logFile)
     .then(() => {
       return new Promise((resolve, reject) => {
-        const cmd = `${zapierCmd} init ${template} --template=${template} --debug && cd ${template} && npm install ${extraCmd} && ${zapierCmd} validate && export CLIENT_ID=1234 CLIENT_SECRET=asdf && ${zapierCmd} test --timeout=5000`;
-        const child = childProcess.exec(cmd, { cwd: rootTmpDir }, err => {
+        const cmd = `${zapierCmd} init ${template} --template=${template} --debug && cd ${template} && npm install && ${zapierCmd} validate && export CLIENT_ID=1234 CLIENT_SECRET=asdf && ${zapierCmd} test --timeout=5000`;
+        const child = childProcess.exec(cmd, { cwd: rootTmpDir }, (err) => {
           if (err) {
             console.log('error starting child process:', err);
             reject(err);
@@ -53,12 +52,12 @@ const rootTmpDir = tmp.tmpNameSync();
 fse.removeSync(rootTmpDir);
 fse.ensureDirSync(rootTmpDir);
 
-const tasks = _.map(appTemplates, template =>
+const tasks = _.map(appTemplates, (template) =>
   validateAppTemplate(template, rootTmpDir)
 );
 
-Promise.all(tasks).then(results => {
-  const failures = _.filter(results, result => result !== null);
+Promise.all(tasks).then((results) => {
+  const failures = _.filter(results, (result) => result !== null);
   if (failures.length) {
     console.error(
       'these app templates failed to validate:',
