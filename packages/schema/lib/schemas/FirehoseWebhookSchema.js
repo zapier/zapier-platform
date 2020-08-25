52f99553a9e315a0aa97580091bb53a8b9310db1
Cass Hill <cass.hill@zapier.com>
2020-08-25 16:47:10 +1000

    cleanup schema

--- packages/schema/lib/schemas/FirehoseWebhookSchema.js
+++ packages/schema/lib/schemas/FirehoseWebhookSchema.js
@@ -15,46 +15,11 @@ module.exports = makeSchema(
     properties: {
       performSubscriptionKeyList: {
         description:
-          'Takes a webhook payload and returns the identifier required to lookup subscribed Zaps.',
+          'Takes a webhook payload and returns the list of subscription keys required to lookup subscribed Zaps.',
         $ref: FunctionSchema.id,
       },
-      // ideas from the ptototype, will be added in incrementally
-      // dedupe: {
-      //   description:
-      //     'set to true if your API sends duplicate webhook events, and Zapier needs to filter duplicates',
-      //   type: 'boolean'
-      // },
-      // batchKey: {
-      //   description:
-      //     'if your API sends webhooks in batches (ie a list/array of events) and the events are nested under a json key, provide the key',
-      //   type: 'string'
-      // },
-      // challenge: {
-      //   description:
-      //     'For APIs that send challenge payloads to the webhook endpoint, as well as webhook events',
-      //   type: 'object',
-      //   required: ['sendsChallenge'],
-      //   properties: {
-      //     sendsChallenge: {
-      //       description:
-      //         'does the API send challenges to the webhook endpoint, as well as the expected webhook events?',
-      //       type: 'boolean'
-      //     },
-      //     location: {
-      //       description:
-      //         'where in the request we should look to find the challenge value',
-      //       type: 'string',
-      //       enum: ['query_string', 'body']
-      //     },
-      //     key: {
-      //       description:
-      //         "the key containing the challenge value. We'll response to the request with this value",
-      //       type: 'string'
-      //     }
-      //   }
-      // }
     },
-    additionalProperties: true,
+    additionalProperties: false,
   },
   [FunctionSchema]
 );
