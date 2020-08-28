da49aff8ce89171267c81563e7e082328ca0c8c5
Cass Hill <cass.hill@zapier.com>
2020-08-28 13:15:21 +1000

    remove the counter for firehose schema in tests

--- packages/schema/test/index.js
+++ packages/schema/test/index.js
@@ -9,7 +9,7 @@ const appDefinition = require('../examples/definition.json');
 
 const copy = (o) => JSON.parse(JSON.stringify(o));
 
-const NUM_SCHEMAS = 49; // changes regularly as we expand
+const NUM_SCHEMAS = 48; // changes regularly as we expand
 
 describe('app', () => {
   describe('validation', () => {
