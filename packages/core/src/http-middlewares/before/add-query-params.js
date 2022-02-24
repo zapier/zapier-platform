417eeaff565c7641db2ddba886a0dce3a4b941fc
David Brownman <beamneocube@gmail.com>
2022-02-24 15:44:31 -0800

    enable skipping encoding specific characters from query params

--- packages/core/src/http-middlewares/before/add-query-params.js
+++ packages/core/src/http-middlewares/before/add-query-params.js
@@ -14,7 +14,23 @@ const addQueryParams = (req) => {
 
     normalizeEmptyParamFields(req);
 
-    const stringifiedParams = querystring.stringify(req.params);
+    let stringifiedParams = querystring.stringify(req.params);
+
+    // it goes against spec, but for compatibility, some APIs want certain characters (mostly $) unencoded
+    (req.skipEncodingChars || []).forEach((char) => {
+      if (char.length > 1) {
+        throw new Error('only pass single characters to skipEncodingChars');
+      }
+      const valToReplace = querystring.escape(char);
+      if (valToReplace === char) {
+        return;
+      }
+      // no replaceAll in JS yet, coming in a node version soon!
+      stringifiedParams = stringifiedParams.replace(
+        new RegExp(valToReplace, 'g'),
+        char
+      );
+    });
 
     if (stringifiedParams) {
       req.url += `${splitter}${stringifiedParams}`;
