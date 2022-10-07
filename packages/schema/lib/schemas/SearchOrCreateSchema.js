13913aaa5b88ebddb894551a68de30e0a086f365
Raúl Negrón <raul.negron@zapier.com>
2022-10-07 16:10:53 -0400

    initial commit adding new keys to search-or-create

--- packages/schema/lib/schemas/SearchOrCreateSchema.js
+++ packages/schema/lib/schemas/SearchOrCreateSchema.js
@@ -3,6 +3,7 @@
 const makeSchema = require('../utils/makeSchema');
 
 const BasicDisplaySchema = require('./BasicDisplaySchema');
+const FlatObjectSchema = require('./FlatObjectSchema');
 const KeySchema = require('./KeySchema');
 
 module.exports = makeSchema(
@@ -30,6 +31,18 @@ module.exports = makeSchema(
         description: 'The key of the create that powers this search-or-create',
         $ref: KeySchema.id,
       },
+      update: {
+        description: 'The key of the create that will be used if a search succeeds, having a value here converts this search-or-create into a search-and-create',
+        $ref: KeySchema.id,
+      },
+      updateInputFromSearchOutput: {
+        description: 'A mapping where the key represents the input field for the update action, and the value represents the field from the search action\'s output that should be mapped to the update action\'s input field.',
+        $ref: FlatObjectSchema.id,
+      },
+      searchUniqueInputToOutputConstraint: {
+        description: 'A mapping where the key represents an input field for the search action, and the value represents how that field\'s value will be used to filter down the search output for an exact match.',
+        $ref: FlatObjectSchema.id,
+      }
     },
     additionalProperties: false,
     examples: [
@@ -44,6 +57,25 @@ module.exports = makeSchema(
         },
         search: 'searchWidgets',
         create: 'createWidget',
+      },
+      {
+        key: 'upsertWidgets',
+        display: {
+          label: 'Upsert Widgets',
+          description:
+            'Searches for a widget matching the provided query and updates it if found, or creates one if it does not exist.',
+          important: true,
+          hidden: false,
+        },
+        search: 'searchWidgets',
+        create: 'createWidget',
+        update: 'updateExistingWidget',
+        updateInputFromSearchOutput: {
+          widget_id: 'id'
+        },
+        searchUniqueInputToOutputConstraint: {
+          widget_name: 'name'
+        }
       },
     ],
     antiExamples: [
@@ -78,6 +110,28 @@ module.exports = makeSchema(
         reason:
           'Invalid values for keys: search and create (must be a string that matches the key of a registered search or create)',
       },
+      {
+        example: {
+          key: 'upsertWidgets',
+          display: {
+            label: 'Upsert Widgets',
+            description:
+              'Searches for a widget matching the provided query and updates it if found, or creates one if it does not exist.',
+            important: true,
+            hidden: false,
+          },
+          search: 'searchWidgets',
+          create: 'createWidget',
+          updateInputFromSearchOutput: {
+            widget_id: 'id'
+          },
+          searchUniqueInputToOutputConstraint: {
+            widget_name: 'name'
+          },
+        },
+        reason:
+          'If either the updateInputFromSearchOutput or searchUniqueInputToOutputConstraint keys are present, then the update key must be present as well.',
+      }
     ],
   },
   [BasicDisplaySchema, KeySchema]
