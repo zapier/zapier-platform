c884cf31b4452d5aa1398a714aa826bbc586353c
Chang-Hung Liang <chang-hung.liang@zapier.com>
2023-01-13 15:13:55 +0800

    Remove debugger

--- packages/schema/lib/functional-constraints/searchOrCreateKeys.js
+++ packages/schema/lib/functional-constraints/searchOrCreateKeys.js
@@ -185,7 +185,6 @@ const validateSearchOrCreateKeys = (definition) => {
           );
         }
 
-        debugger;
         if (
           (hasSearchOutputFields || hasSearchOutputSample) &&
           !allSearchOutputKeys.has(searchOutputField)
