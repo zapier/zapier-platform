af927cdd67b4b2b6391c41546c41997108d435b7
Greg Aiello <greg.aiello@zapier.com>
2023-02-06 14:03:06 -0500

    Mock API call for register field choices when testing

--- packages/cli/src/oclif/hooks/getAppRegistrationFieldChoices.js
+++ packages/cli/src/oclif/hooks/getAppRegistrationFieldChoices.js
@@ -1,9 +1,18 @@
 const { callAPI } = require('../../utils/api');
+const { BASE_ENDPOINT, IS_TESTING } = require('../../constants');
 
 module.exports = async function (options) {
   // We only need to run this for the register command
   if (!options || !options.id || options.id !== 'register') {
     return null;
+  }
+
+  // Mock API call for register field choices when testing
+  if (IS_TESTING) {
+    const registerFieldChoices = require('../fixtures/registerFieldChoices');
+    require('nock')(BASE_ENDPOINT)
+      .get('/api/platform/cli/apps/fields-choices')
+      .reply(200, registerFieldChoices);
   }
 
   const enumFieldChoices = {};
