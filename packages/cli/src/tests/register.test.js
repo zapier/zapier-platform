c9ebfe474189855794f098b60c331501ef0c2467
Greg Aiello <greg.aiello@zapier.com>
2023-02-14 17:36:34 -0500

    Add test to ensure flags work properly for zapier register command
    * This commit also utilizes nock for API requests in "zapier register should enforce character limits on flags" test cases

--- packages/cli/src/tests/register.test.js
+++ packages/cli/src/tests/register.test.js
@@ -1,10 +1,29 @@
+const fs = require('fs');
 const oclif = require('@oclif/test');
 const { BASE_ENDPOINT, MAX_DESCRIPTION_LENGTH } = require('../constants');
 const registerFieldChoices = require('./fixtures/registerFieldChoices');
+const createApp = require('./fixtures/createApp');
+const { after } = require('mocha');
 
 describe('RegisterCommand', () => {
+  // Delete generated .zapierapprc file after all tests run
+  after(() => {
+    const apprcFile = './.zapierapprc';
+    if (fs.statSync(apprcFile, { throwIfNoEntry: false })) {
+      fs.unlinkSync(apprcFile);
+    }
+  });
+
+  function getTestObj() {
+    return oclif.test.nock(BASE_ENDPOINT, (mockApi) =>
+      mockApi
+        .get('/api/platform/cli/apps/fields-choices')
+        .reply(200, registerFieldChoices)
+    );
+  }
+
   describe('zapier register should enforce character limits on flags', function () {
-    oclif.test
+    getTestObj()
       .command([
         'register',
         '--desc',
@@ -21,14 +40,6 @@ describe('RegisterCommand', () => {
   });
 
   describe('zapier register should validate enum fields that are passed in as flags', function () {
-    function getTestObj() {
-      return oclif.test.nock(BASE_ENDPOINT, (mockApi) =>
-        mockApi
-          .get('/api/platform/cli/apps/fields-choices')
-          .reply(200, registerFieldChoices)
-      );
-    }
-
     getTestObj()
       .command(['register', '--role', 'invalidRole'])
       .catch((ctx) => {
@@ -55,5 +66,38 @@ describe('RegisterCommand', () => {
           .to.contain('invalidAudience is not a valid value for audience');
       })
       .it('zapier register should throw error for invalid audience');
+  });
+
+  describe('zapier register should accept all data via flags', function () {
+    function getTestObj() {
+      return oclif.test.nock(BASE_ENDPOINT, (mockApi) =>
+        mockApi
+          .get('/api/platform/cli/apps/fields-choices')
+          .reply(200, registerFieldChoices)
+          .post('/api/platform/cli/apps')
+          .query({ formId: 'create' })
+          .reply(201, createApp)
+      );
+    }
+
+    getTestObj()
+      .command([
+        'register',
+        '"My Cool Integration"',
+        '--desc',
+        '"My Cool Integration helps you integrate your apps with the apps that you need."',
+        '--url',
+        '"https://www.zapier.com"',
+        '--audience',
+        'private',
+        '--role',
+        'employee',
+        '--category',
+        'marketing-automation',
+        '--subscribe',
+      ])
+      .it(
+        'zapier register should successfully register an app with all data provided'
+      );
   });
 });
