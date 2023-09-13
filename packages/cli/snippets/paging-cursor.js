cd7185b4076416fd2d17b42740f07bfe8b4f013d
Raúl Negrón <rnegron@users.noreply.github.com>
2023-09-13 10:10:26 -0400

    [INT-18028] docs(cli): Bring over paging cursor doc changes added in visual-builder repo (#701)
    Co-authored-by: Raúl Negrón <raul.negron@zapier.com>

--- packages/cli/snippets/paging-cursor.js
+++ packages/cli/snippets/paging-cursor.js
@@ -1,51 +1,29 @@
-// the perform method of our trigger
-// ensure operation.canPaginate is true!
-
-const performWithoutAsync = (z, bundle) => {
-  return Promise.resolve()
-    .then(() => {
-      if (bundle.meta.page === 0) {
-        // first page, no need to fetch a cursor
-        return Promise.resolve();
-      } else {
-        return z.cursor.get(); // Promise<string | null>
-      }
-    })
-    .then((cursor) => {
-      return z.request(
-        'https://5ae7ad3547436a00143e104d.mockapi.io/api/recipes',
-        {
-          params: { cursor }, // if cursor is null, it's ignored here
-        }
-      );
-    })
-    .then((response) => {
-      // need to save the cursor and return a promise, but also need to pass the data along
-      return Promise.all([response.items, z.cursor.set(response.nextPage)]);
-    })
-    .then(([items /* null */]) => {
-      return items;
-    });
-};
-
-// ---------------------------------------------------
-
-const performWithAsync = async (z, bundle) => {
+const perform = async (z, bundle) => {
   let cursor;
-  if (bundle.meta.page) {
-    cursor = await z.cursor.get(); // string | null
+
+  // if fetching a page other than the first (first page is 0),
+  // get the cursor stored after fetching the previous page.
+  if (bundle.meta.page > 0) {
+    cursor = await z.cursor.get();
+
+    // if the previous page was the last one and cursor is empty/null,
+    // return an empty array.
+    if (!cursor) {
+      return [];
+    }
   }
 
   const response = await z.request(
     'https://5ae7ad3547436a00143e104d.mockapi.io/api/recipes',
     {
-      // if cursor is null, it's sent as an empty query
-      //   param and should be ignored by the server
+      // cursor typically is a param to pass along to the next request,
+      // or the full URL for the next page of items.
       params: { cursor },
     }
   );
 
-  // we successfully got page 1, should store the cursor in case the user wants page 2
+  // after fetching a page, set the returned cursor for the next page,
+  // or empty/null if this was the last one.
   await z.cursor.set(response.nextPage);
 
   return response.items;
