ebd4ce2ec2daf4a39d1ba014006bbda27936a902
Stanley Chin <stan.chin@zapier.com>
2024-08-13 16:56:06 -0400

    Add a tip before running pull

--- packages/cli/src/generators/pull.js
+++ packages/cli/src/generators/pull.js
@@ -1,5 +1,6 @@
 const colors = require('colors/safe');
 const debug = require('debug')('zapier:pull');
+const inquirer = require('inquirer');
 const path = require('path');
 const Generator = require('yeoman-generator');
 
@@ -24,6 +25,28 @@ module.exports = class PullGenerator extends Generator {
   initializing() {
     debug('SRC', this.options.sourceFiles);
     debug('DEL', this.options.deletableFiles);
+  }
+
+  prompting() {
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: `Warning: You are about to overwrite existing files.
+
+Before proceeding, please make sure you have saved your work. Consider creating a backup or saving your current state in a git branch. During the process, you may abort anytime by pressing 'a'. 
+
+Do you want to continue?`,
+        default: false,
+      },
+    ];
+
+    return inquirer.prompt(prompts).then((answers) => {
+      if (!answers.confirm) {
+        this.log(colors.green('zapier pull cancelled'));
+        process.exit(1);
+      }
+    });
   }
 
   writing() {
