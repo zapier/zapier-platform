bd6fe96598f636928cf8784438686cecb71dca35
Stanley Chin <stan.chin@zapier.com>
2024-08-14 12:06:00 -0400

    compare every file

--- packages/cli/src/generators/pull.js
+++ packages/cli/src/generators/pull.js
@@ -5,14 +5,9 @@ const path = require('path');
 const Generator = require('yeoman-generator');
 
 const maybeOverwriteFiles = async (gen) => {
-  const existingFileSet = new Set(gen.options.deletableFiles);
   const delDir = gen.options.delDir;
   const srcDir = gen.options.srcDir;
   for (const file of gen.options.sourceFiles) {
-    // File not in source, we don't have to worry about it
-    if (!existingFileSet.has(file)) {
-      continue;
-    }
     gen.fs.copyTpl(
       gen.templatePath(path.join(srcDir, file)),
       gen.destinationPath(path.join(delDir, file)),
