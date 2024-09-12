368edd812c183d908641daf41e5298bc706f533b
Zac Jones <zac.jones@zapier.com>
2024-09-12 14:00:32 -0500

    Cleanups and shuffle logic for better flow

--- packages/cli/src/oclif/commands/canary.js
+++ packages/cli/src/oclif/commands/canary.js
@@ -2,27 +2,60 @@ const ZapierBaseCommand = require('../ZapierBaseCommand');
 
 const { buildFlags } = require('../buildFlags');
 const { flags } = require('@oclif/command');
-const { handleCreate, handleList, handleDelete } = require('../../utils/canary');
+const { grey, bold } = require('colors/safe');
+const { createCanary, listCanaries, deleteCanary } = require('../../utils/api');
 
 class CanaryCommand extends ZapierBaseCommand {
   async perform() {
-    try {
-      if (this.flags.create) {
-        await handleCreate(
-          this.flags.versionFrom,
-          this.flags.versionTo,
-          this.flags.percentage,
-          this.flags.duration
-        );
-      } else if (this.flags.list) {
-        await handleList();
-      } else if (this.flags.delete) {
-        await handleDelete(this.flags.versionFrom, this.flags.versionTo);
-      } else {
-        console.log('Please specify a canary command: --create, --list, or --delete');
+    const { versionFrom, versionTo, percentage, duration } = this.flags;
+
+    if (this.flags.create) {
+      if (!versionFrom || !versionTo || !percentage || !duration) {
+        this.error('All parameters are required for creating a canary deployment: versionFrom, versionTo, percentage, duration');
       }
-    } catch (error) {
-      this.error(error.message);
+
+      this.startSpinner(`Creating canary deployment
+    - From version: ${versionFrom}
+    - To version: ${versionTo}
+    - Percentage: ${percentage}%
+    - Duration: ${duration} seconds`
+      )
+
+      await createCanary(versionFrom, versionTo, percentage, duration);
+
+      this.stopSpinner();
+    } else if (this.flags.list) {
+      const canaries = await listCanaries();
+
+      const formattedCanaries = canaries.objects.map(c => ({
+        from_version: c.from_version,
+        to_version: c.to_version,
+        percent: c.percent,
+        seconds_remaining: c.until_timestamp - Math.floor(Date.now() / 1000)
+      }));
+
+      this.log(bold('Active Canaries') + '\n');
+      this.logTable({
+        rows: formattedCanaries,
+        headers: [
+          ['From Version', 'from_version'],
+          ['To Version', 'to_version'],
+          ['Traffic Percent', 'percent'],
+          ['Seconds Remaining', 'seconds_remaining'],
+        ],
+        emptyMessage: grey(
+          `No active canary deployments found.`
+        ),
+      })
+
+    } else if (this.flags.delete) {
+      if (!versionFrom || !versionTo) {
+        this.error('The versionFrom and versionTo parameters are required for deleting a canary deployment');
+      }
+
+      this.startSpinner(`Deleting active canary from ${versionFrom} to ${versionTo}`);
+      await deleteCanary(this.flags.versionFrom, this.flags.versionTo);
+      this.stopSpinner();
     }
   }
 }
@@ -36,6 +69,9 @@ CanaryCommand.flags = buildFlags({
     'versionTo': flags.string({char: 't', description: 'Version to canary traffic to'}),
     'percentage': flags.integer({char: 'p', description: 'Percentage of traffic to route to new version'}),
     'duration': flags.integer({char: 's', description: 'Duration of the canary in seconds'}),
+  },
+  opts: {
+    format: true
   }
 });
 
