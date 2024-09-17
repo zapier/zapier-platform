3298b55ed71d090a205d39a37d7187465138ed73
Chang-Hung Liang <chang-hung.liang@zapier.com>
2024-09-17 12:28:18 +0800

    Update version tag

--- packages/core/types/zapier.generated.d.ts
+++ packages/core/types/zapier.generated.d.ts
@@ -4,7 +4,7 @@
  * files, and/or the schema-to-ts tool and run its CLI to regenerate
  * these typings.
  *
- * zapier-platform-schema version: 15.14.1
+ * zapier-platform-schema version: 15.15.0
  *  schema-to-ts compiler version: 0.1.0
  */
 
