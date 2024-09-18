1abb24845329a24ae54b1f23ce976acfa38bb573
Zac Jones <zac.jones@zapier.com>
2024-09-18 16:00:45 -0500

    Move percent and duration to flags

--- packages/cli/src/oclif/commands/canary/create.js
+++ packages/cli/src/oclif/commands/canary/create.js
@@ -1,9 +1,13 @@
 const ZapierBaseCommand = require('../../ZapierBaseCommand');
 const { createCanary, listCanaries } = require('../../../utils/api');
+const { buildFlags } = require('../../buildFlags');
+const { flags } = require('@oclif/command');
 
 class CanaryCreateCommand extends ZapierBaseCommand {
   async perform() {
-    const { versionFrom, versionTo, percent, duration } = this.args;
+    const { versionFrom, versionTo } = this.args;
+    const percent = this.flags.percent;
+    const duration = this.flags.duration;
 
     this.validateVersions(versionFrom, versionTo);
     this.validatePercent(percent);
@@ -43,16 +47,23 @@ If you would like to stop this canary now, run \`zapier canary:delete ${existing
 
   validatePercent(percent) {
     if (isNaN(percent) || percent < 1 || percent > 100) {
-      this.error('`PERCENT` must be a number between 1 and 100');
+      this.error('`--percent` must be a number between 1 and 100');
     }
   }
 
   validateDuration(duration) {
     if (isNaN(duration) || duration < 30 || duration > 24 * 60 * 60) {
-      this.error('`DURATION` must be a positive number between 30 and 86400');
+      this.error('`--duration` must be a positive number between 30 and 86400');
     }
   }
 }
+
+CanaryCreateCommand.flags = buildFlags({
+  commandFlags: {
+    percent: flags.integer({char: 'p', description: 'Percent of traffic to route to new version', required: true}),
+    duration: flags.integer({char: 'd', description: 'Duration of the canary in seconds', required: true}),
+  }
+})
 
 CanaryCreateCommand.args = [
   {
@@ -65,32 +76,19 @@ CanaryCreateCommand.args = [
     required: true,
     description: 'Version to canary traffic to',
   },
-  {
-    name: 'percent',
-    required: true,
-    description: 'Percentage (between 1 and 100) of traffic to route to new version',
-    parse: (input) => parseInt(input, 10),
-  },
-  {
-    name: 'duration',
-    required: true,
-    description: 'Duration (between 30 and 86400) of the canary in seconds',
-    parse: (input) => parseInt(input, 10),
-  },
 ];
 
-CanaryCreateCommand.description =
-  'Create a new canary deployment, diverting a specified percentage of traffic from one version to another for a specified duration. \n' +
-  '\n' +
-  'Only one canary can be active at the same time. You can run `zapier canary:list` to check. If you would like to create a new canary with different parameters, you can wait for the canary to finish, or delete it using `zapier canary:delete a.b.c x.y.z`. \n' +
-  '\n' +
-  'Note: this is similar to `zapier migrate` but different in that this is temporary and will "revert" the changes once the specified duration is expired. \n' +
-  '\n' +
-  '**Only use this command to canary traffic between non-breaking versions!**';
+CanaryCreateCommand.description = `Create a new canary deployment, diverting a specified percentage of traffic from one version to another for a specified duration.
+
+Only one canary can be active at the same time. You can run \`zapier canary:list\` to check. If you would like to create a new canary with different parameters, you can wait for the canary to finish, or delete it using \`zapier canary:delete a.b.c x.y.z\`.
+
+Note: this is similar to \`zapier migrate\` but different in that this is temporary and will "revert" the changes once the specified duration is expired.
+
+**Only use this command to canary traffic between non-breaking versions!**`;
 
 CanaryCreateCommand.examples = [
-  'zapier canary:create 1.0.0 1.1.0 25 720',
-  'zapier canary:create 2.0.0 2.1.0 50 300'
+  'zapier canary:create 1.0.0 1.1.0 -p 25 -d 720',
+  'zapier canary:create 2.0.0 2.1.0 --percent 50 --duration 300'
 ];
 CanaryCreateCommand.skipValidInstallCheck = true;
 
