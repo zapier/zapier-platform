d6b2a96fa6bedb26d5fc352f1633dd4d7a6f5177
Thomas Cranny <thomas.cranny@zapier.com>
2024-10-24 16:25:28 +1000

    Update all four TS templates

--- packages/cli/scaffold/create.template.ts
+++ packages/cli/scaffold/create.template.ts
@@ -1,37 +1,43 @@
-import type { PerformFunction, Trigger } from 'zapier-platform-core';
+import type { Create, PerformFunction } from 'zapier-platform-core';
 
-// triggers on a new <%= LOWER_NOUN %> with a certain tag
+// create a particular <%= LOWER_NOUN %> by name
 const perform: PerformFunction = async (z, bundle) => {
   const response = await z.request({
+    method: 'POST',
     url: 'https://jsonplaceholder.typicode.com/posts',
-    params: {
-      tag: bundle.inputData.tagName,
-    },
+    // if `body` is an object, it'll automatically get run through JSON.stringify
+    // if you don't want to send JSON, pass a string in your chosen format here instead
+    body: {
+      name: bundle.inputData.name
+    }
   });
-  // this should return an array of objects
+  // this should return a single object
   return response.data;
 };
 
 export default {
   // see here for a full list of available properties:
-  // https://github.com/zapier/zapier-platform/blob/main/packages/schema/docs/build/schema.md#triggerschema
-  key: '<%= KEY %>' as const,
+  // https://github.com/zapier/zapier-platform/blob/main/packages/schema/docs/build/schema.md#createschema
+  key: '<%= KEY %>',
   noun: '<%= NOUN %>',
 
   display: {
-    label: 'New <%= NOUN %>',
-    description: 'Triggers when a new <%= LOWER_NOUN %> is created.',
+    label: 'Create <%= NOUN %>',
+    description: 'Creates a new <%= LOWER_NOUN %>, probably with input from previous steps.'
   },
 
   operation: {
-    type: 'polling',
     perform,
 
     <%= INCLUDE_INTRO_COMMENTS ? [
       '// `inputFields` defines the fields a user could provide',
-      '// Zapier will pass them in as `bundle.inputData` later. They\'re optional.'
+      '// Zapier will pass them in as `bundle.inputData` later. They\'re optional.',
+      '// End-users will map data into these fields. In general, they should have any fields that the API can accept. Be sure to accurately mark which fields are required!'
     ].join('\n    ') : '' %>
-    inputFields: [],
+    inputFields: [
+      {key: 'name', required: true},
+      {key: 'fave_meal', label: 'Favorite Meal', required: false}
+    ],
 
     <%= INCLUDE_INTRO_COMMENTS ? [
       '// In cases where Zapier needs to show an example record to the user, but we are unable to get a live example',
@@ -40,7 +46,7 @@ export default {
     ].join('\n    ') : '' %>
     sample: {
       id: 1,
-      name: 'Test',
+      name: 'Test'
     },
 
     <%= INCLUDE_INTRO_COMMENTS ? [
@@ -53,6 +59,6 @@ export default {
       // these are placeholders to match the example `perform` above
       // {key: 'id', label: 'Person ID'},
       // {key: 'name', label: 'Person Name'}
-    ],
-  },
-} satisfies Trigger;
+    ]
+  }
+} satisfies Create;
