eb333e9af076ffbb8bae2bf8b4ca48821daf8b8f
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2025-02-04 14:18:34 -0800

    re-add index.js

--- packages/core/include/zapierwrapper.js
+++ packages/core/include/zapierwrapper.js
@@ -1,6 +1,7 @@
 // not intended to be loaded via require() - copied during build step
+import path from 'node:path';
 import zapier from 'zapier-platform-core';
 
-const appPath = process.cwd();
+const appPath = path.resolve(process.cwd(), 'index.js');
 
 export const handler = zapier.createAppHandler(appPath);
