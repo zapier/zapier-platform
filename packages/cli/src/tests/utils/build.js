a1d315da84d01a44778c669ac462f63ef3416acc
Natay Aberra <natay.aberra@zapier.com>
2025-02-18 13:51:11 -0500

    fix test

--- packages/cli/src/tests/utils/build.js
+++ packages/cli/src/tests/utils/build.js
@@ -59,7 +59,7 @@ describe('build (runs slowly)', function () {
     smartPaths.filter((p) => p.endsWith('.ts')).length.should.equal(0);
     smartPaths.should.not.containEql('tsconfig.json');
 
-    smartPaths.length.should.be.within(200, 304);
+    smartPaths.length.should.be.within(200, 306);
   });
 
   it('should list all the files', () => {
