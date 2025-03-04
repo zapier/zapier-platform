d11df60c1c3cf0e2a1481e970b1ba45c65576e85
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2025-03-04 15:34:33 -0800

    retrieve package name from package.json

--- packages/core/include/zapierwrapper.mjs
+++ packages/core/include/zapierwrapper.mjs
@@ -1,7 +1,12 @@
 // not intended to be loaded via require() or import() - copied during build step
 import zapier from 'zapier-platform-core';
+import { promises as fs } from 'fs';
+import path from 'node:path';
 
-// TODO: Find the package name in package.json instead of hardcoding "esmodule-app"
+const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+const packageJson = JSON.parse(await fs.readFile(packageJsonPath, { encoding: 'utf8' }));
+const packageName = packageJson.name;
+
 // https://nodejs.org/docs/latest-v22.x/api/all.html#all_packages_self-referencing-a-package-using-its-name
-const appRaw = (await import('esmodule-app')).default;
+const appRaw = (await import(packageName)).default;
 export const handler = zapier.createAppHandler(appRaw);
