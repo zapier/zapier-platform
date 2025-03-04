4b7cfab18a07f95cf57119b88b206e48db14deea
Chang-Hung Liang <chang-hung.liang@zapier.com>
2025-03-04 21:21:44 +0800

    Add TODO

--- packages/core/include/zapierwrapper.mjs
+++ packages/core/include/zapierwrapper.mjs
@@ -1,5 +1,7 @@
 // not intended to be loaded via require() or import() - copied during build step
 import zapier from 'zapier-platform-core';
 
+// TODO: Find the package name in package.json instead of hardcoding "esmodule-app"
+// https://nodejs.org/docs/latest-v22.x/api/all.html#all_packages_self-referencing-a-package-using-its-name
 const appRaw = (await import('esmodule-app')).default;
 export const handler = zapier.createAppHandler(appRaw);
