d60c8a5e9622089b874ce70f641778be1ea967dd
Natay Aberra <natay.aberra@gmail.com>
2025-03-05 09:35:39 -0500

    PDE-5810 add copy to AuthFieldSchema (#977)

--- packages/schema/lib/schemas/AuthFieldSchema.js
+++ packages/schema/lib/schemas/AuthFieldSchema.js
@@ -29,7 +29,7 @@ module.exports = makeSchema(
       type: {
         description: 'The type of this value used to be.',
         type: 'string',
-        enum: ['string', 'number', 'boolean', 'datetime', 'password'],
+        enum: ['string', 'number', 'boolean', 'datetime', 'copy', 'password'],
       },
       required: {
         description:
