e0b0ba8d1a26967a857b0ef39c088ac48c21d616
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2025-03-20 14:46:02 -0700

    update test required files number

--- packages/cli/src/tests/utils/build.js
+++ packages/cli/src/tests/utils/build.js
@@ -568,7 +568,7 @@ describe('build ESM (runs slowly)', function () {
     smartPaths.filter((p) => p.endsWith('.ts')).length.should.equal(0);
     smartPaths.should.not.containEql('tsconfig.json');
 
-    smartPaths.length.should.be.within(200, 301);
+    smartPaths.length.should.be.within(200, 304);
   });
 
   it('should list all the files', () => {
