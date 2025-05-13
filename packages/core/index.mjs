ce0fba9b02075ea36c5ba49e172c8b9ee5aa042a
Thomas Cranny <thomas.cranny@zapier.com>
2025-05-14 06:57:43 +1000

    Update index.mjs with advice

--- packages/core/index.mjs
+++ packages/core/index.mjs
@@ -1,5 +1,9 @@
 import zapier from './src/index.js';
-
+import packageJson from './package.json' with { type: 'json' };
+import _tools from './src/tools/exported.js';
+zapier.version = packageJson.version;
+zapier.tools = _tools;
+// Allows `import { ... } from 'zapier-platform-core'`
 export const {
   createAppHandler,
   createAppTester,
@@ -10,4 +14,8 @@ export const {
   defineSearch,
   defineTrigger,
   integrationTestHandler,
+  tools,
+  version,
 } = zapier;
+// Allows `import zapier from 'zapier-platform-core'`
+export default zapier;
