479de0ebade7b84adacdd817e456fd2417557b8d
Thomas Cranny <thomas.cranny@zapier.com>
2025-05-13 16:17:23 +1000

    Provide named esm exports of zpc objects
    This fixes issues where downstream apps can't access members of the
    zapier library via deconstructed import statements.

---
+++ packages/core/index.mjs
@@ -0,0 +1,13 @@
+import zapier from './src/index.js';
+
+export const {
+  createAppHandler,
+  createAppTester,
+  defineApp,
+  defineCreate,
+  defineInputField,
+  defineInputFields,
+  defineSearch,
+  defineTrigger,
+  integrationTestHandler,
+} = zapier;
