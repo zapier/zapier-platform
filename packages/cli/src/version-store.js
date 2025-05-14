819c5f1d09db2f5c955eec569f356a63b843a9d1
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2025-05-14 15:07:58 -0700

    fix CLI 16 and Core 17 incompatibility (#1020)

--- packages/cli/src/version-store.js
+++ packages/cli/src/version-store.js
@@ -18,4 +18,5 @@ module.exports = [
   { nodeVersion: '16', npmVersion: '>=5.6.0' }, // 13.x
   { nodeVersion: '18', npmVersion: '>=5.6.0' }, // 15.x
   { nodeVersion: '18', npmVersion: '>=10.7.0' }, // 16.x
+  { nodeVersion: '18', npmVersion: '>=10.7.0' }, // 17.x
 ];
