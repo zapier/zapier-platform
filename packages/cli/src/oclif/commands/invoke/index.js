8d3f8e0775866ab8f82b0b9de549c6bfd3822ee7
Chang-Hung Liang <chang-hung.liang@zapier.com>
2025-12-26 15:07:05 +0800

    Move resolveInputDataTypes() and related stuff to its own module

--- packages/cli/src/oclif/commands/invoke/index.js
+++ packages/cli/src/oclif/commands/invoke/index.js
@@ -12,10 +12,6 @@ const { Args, Flags } = require('@oclif/core');
 const debug = require('debug')('zapier:invoke');
 const dotenv = require('dotenv');
 
-// Datetime related imports
-const chrono = require('chrono-node');
-const { DateTime, IANAZone } = require('luxon');
-
 const BaseCommand = require('../../ZapierBaseCommand');
 const { buildFlags } = require('../../buildFlags');
 const { localAppCommand } = require('../../../utils/local');
@@ -26,6 +22,7 @@ const {
   readCredentials,
 } = require('../../../utils/api');
 const { AUTH_KEY } = require('../../../constants');
+const resolveInputDataTypes = require('./input-types');
 
 const ACTION_TYPE_PLURALS = {
   trigger: 'triggers',
@@ -36,28 +33,6 @@ const ACTION_TYPE_PLURALS = {
 const ACTION_TYPES = ['auth', ...Object.keys(ACTION_TYPE_PLURALS)];
 
 const AUTH_FIELD_ENV_PREFIX = 'authData_';
-
-const NUMBER_CHARSET = '0123456789.-,';
-
-const FALSE_STRINGS = new Set([
-  'noo',
-  'no',
-  'n',
-  'false',
-  'nope',
-  'f',
-  'never',
-  'no thanks',
-  'no thank you',
-  'nul',
-  '0',
-  'none',
-  'nil',
-  'nill',
-  'null',
-]);
-
-const TRUE_STRINGS = new Set([['yes', 'yeah', 'y', 'true', 't', '1']]);
 
 const loadAuthDataFromEnv = () => {
   return Object.entries(process.env)
@@ -88,160 +63,6 @@ const getMissingRequiredInputFields = (inputData, inputFields) => {
   return inputFields.filter(
     (f) => f.required && !f.default && !inputData[f.key],
   );
-};
-
-const parseDecimal = (s) => {
-  const chars = [];
-  for (const c of s) {
-    if (NUMBER_CHARSET.includes(c)) {
-      chars.push(c);
-    }
-  }
-  const cleaned = chars.join('').replace(/[.,-]$/, '');
-  return parseFloat(cleaned);
-};
-
-const parseInteger = (s) => {
-  const n = parseInt(s);
-  if (!isNaN(n)) {
-    return n;
-  }
-  return Math.floor(parseDecimal(s));
-};
-
-const parseBoolean = (s) => {
-  s = s.toLowerCase();
-  if (TRUE_STRINGS.has(s)) {
-    return true;
-  }
-  if (FALSE_STRINGS.has(s)) {
-    return false;
-  }
-  return Boolean(s);
-};
-
-const parsingCompsToString = (parsingComps) => {
-  const yyyy = parsingComps.get('year');
-  const mm = String(parsingComps.get('month')).padStart(2, '0');
-  const dd = String(parsingComps.get('day')).padStart(2, '0');
-  const hh = String(parsingComps.get('hour')).padStart(2, '0');
-  const ii = String(parsingComps.get('minute')).padStart(2, '0');
-  const ss = String(parsingComps.get('second')).padStart(2, '0');
-  return `${yyyy}-${mm}-${dd}T${hh}:${ii}:${ss}`;
-};
-
-const hasTimeInfo = (parsingComps) => {
-  const tags = [...parsingComps.tags()];
-  for (const tag of tags) {
-    if (tag.includes('ISOFormat') || tag.includes('Time')) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const maybeImplyTimeInfo = (parsingComps) => {
-  if (!hasTimeInfo(parsingComps)) {
-    parsingComps.imply('hour', 9);
-    parsingComps.imply('minute', 0);
-    parsingComps.imply('second', 0);
-    parsingComps.imply('millisecond', 0);
-  }
-  return parsingComps;
-};
-
-const parseTimestamp = (dtString, tzName) => {
-  const match = dtString.match(/-?\d{10,14}/);
-  if (!match) {
-    return null;
-  }
-
-  dtString = match[0];
-  let timestamp = parseInt(dtString);
-  if (dtString.length <= 12) {
-    timestamp *= 1000;
-  }
-
-  return DateTime.fromMillis(timestamp, { zone: tzName }).toFormat(
-    "yyyy-MM-dd'T'HH:mm:ssZZ",
-  );
-};
-
-const parseDatetime = (dtString, tzName, now) => {
-  const timestampResult = parseTimestamp(dtString, tzName);
-  if (timestampResult) {
-    return timestampResult;
-  }
-
-  const offset = IANAZone.create(tzName).offset(now.getTime());
-  const results = chrono.parse(dtString, {
-    instant: now,
-    timezone: offset,
-  });
-
-  let isoString;
-  if (results.length) {
-    const parsingComps = results[0].start;
-    if (parsingComps.get('timezoneOffset') == null) {
-      // No timezone info in the input string => interpret the datetime string
-      // exactly as it is and append the timezone
-      isoString = parsingCompsToString(maybeImplyTimeInfo(parsingComps));
-    } else {
-      // Timezone info is present or implied in the input string => convert the
-      // datetime to the specified timezone
-      isoString = maybeImplyTimeInfo(parsingComps).date().toISOString();
-    }
-  } else {
-    // No datetime info in the input string => just return the current time
-    isoString = now.toISOString();
-  }
-
-  return DateTime.fromISO(isoString, { zone: tzName }).toFormat(
-    "yyyy-MM-dd'T'HH:mm:ssZZ",
-  );
-};
-
-const resolveInputDataTypes = (inputData, inputFields, timezone) => {
-  const fieldsWithDefault = inputFields.filter((f) => f.default);
-  for (const f of fieldsWithDefault) {
-    if (!inputData[f.key]) {
-      inputData[f.key] = f.default;
-    }
-  }
-
-  const inputFieldsByKey = _.keyBy(inputFields, 'key');
-  for (const [k, v] of Object.entries(inputData)) {
-    const inputField = inputFieldsByKey[k];
-    if (!inputField) {
-      continue;
-    }
-
-    switch (inputField.type) {
-      case 'integer':
-        inputData[k] = parseInteger(v);
-        break;
-      case 'number':
-        inputData[k] = parseDecimal(v);
-        break;
-      case 'boolean':
-        inputData[k] = parseBoolean(v);
-        break;
-      case 'datetime':
-        inputData[k] = parseDatetime(v, timezone, new Date());
-        break;
-      case 'file':
-        // TODO: How to handle a file field?
-        break;
-      // TODO: Handle 'list' and 'dict' types?
-      default:
-        // No need to do anything with 'string' type
-        break;
-    }
-  }
-
-  // TODO: Handle line items (fields with "children")
-
-  return inputData;
 };
 
 const appendEnv = async (vars, prefix = '') => {
