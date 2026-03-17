c458fbca26faf8dc6f1f868a1e7e204d8e918400
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2026-03-17 10:38:36 -0700

    update tests, add resource handling

--- packages/schema/lib/functional-constraints/schemaRequiresJsonType.js
+++ packages/schema/lib/functional-constraints/schemaRequiresJsonType.js
@@ -5,6 +5,9 @@ const jsonschema = require('jsonschema');
 
 const PLAIN_INPUT_FIELD_SCHEMA_ID = '/PlainInputFieldSchema';
 
+const actionTypes = ['triggers', 'searches', 'creates', 'bulkReads'];
+const resourceMethods = ['get', 'list', 'hook', 'search', 'create'];
+
 const checkField = (field, path) => {
   if (_.has(field, 'schema') && field.type !== 'json') {
     return [
@@ -30,18 +33,36 @@ const schemaRequiresJsonType = (definition, mainSchema) => {
   }
 
   // Handle full app definition validation
-  _.each(['triggers', 'searches', 'creates'], (typeOf) => {
-    if (definition[typeOf]) {
-      _.each(definition[typeOf], (actionDef) => {
+  _.each(actionTypes, (actionType) => {
+    if (definition[actionType]) {
+      _.each(definition[actionType], (actionDef) => {
         if (actionDef.operation && actionDef.operation.inputFields) {
           _.each(actionDef.operation.inputFields, (field, index) => {
-            const path = `instance.${typeOf}.${actionDef.key}.inputFields[${index}]`;
+            const path = `instance.${actionType}.${actionDef.key}.inputFields[${index}]`;
             errors = errors.concat(checkField(field, path));
           });
         }
       });
     }
   });
+
+  // Handle resources if they exist
+  if (definition.resources) {
+    _.each(definition.resources, (resource, resourceKey) => {
+      resourceMethods.forEach((method) => {
+        if (
+          resource[method] &&
+          resource[method].operation &&
+          resource[method].operation.inputFields
+        ) {
+          _.each(resource[method].operation.inputFields, (field, index) => {
+            const path = `instance.resources.${resourceKey}.${method}.operation.inputFields[${index}]`;
+            errors = errors.concat(checkField(field, path));
+          });
+        }
+      });
+    });
+  }
 
   return errors;
 };
--- packages/schema/test/functional-constraints/schemaRequiresJsonType.js
+++ packages/schema/test/functional-constraints/schemaRequiresJsonType.js
@@ -4,136 +4,179 @@ require('should');
 const schema = require('../../schema');
 
 describe('schemaRequiresJsonType', () => {
-  it('should not error when type is json and schema is provided', () => {
-    const definition = {
-      version: '1.0.0',
-      platformVersion: '1.0.0',
-      creates: {
-        foo: {
-          key: 'foo',
-          noun: 'Foo',
-          display: {
-            label: 'Create Foo',
-            description: 'Creates a...',
-          },
-          operation: {
-            perform: '$func$2$f$',
-            sample: { id: 1 },
-            inputFields: [
-              {
-                key: 'payload',
-                type: 'json',
-                schema: {
-                  type: 'object',
-                  properties: {
-                    name: { type: 'string' },
+  describe('standard action validation', () => {
+    it('should not error when type is json and schema is provided', () => {
+      const definition = {
+        version: '1.0.0',
+        platformVersion: '1.0.0',
+        creates: {
+          foo: {
+            key: 'foo',
+            noun: 'Foo',
+            display: {
+              label: 'Create Foo',
+              description: 'Creates a...',
+            },
+            operation: {
+              perform: '$func$2$f$',
+              sample: { id: 1 },
+              inputFields: [
+                {
+                  key: 'payload',
+                  type: 'json',
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      name: { type: 'string' },
+                    },
                   },
                 },
-              },
-            ],
+              ],
+            },
           },
         },
-      },
-    };
+      };
 
-    const results = schema.validateAppDefinition(definition);
-    results.errors.should.have.length(0);
+      const results = schema.validateAppDefinition(definition);
+      results.errors.should.have.length(0);
+    });
+
+    it('should not error when type is json without schema', () => {
+      const definition = {
+        version: '1.0.0',
+        platformVersion: '1.0.0',
+        creates: {
+          foo: {
+            key: 'foo',
+            noun: 'Foo',
+            display: {
+              label: 'Create Foo',
+              description: 'Creates a...',
+            },
+            operation: {
+              perform: '$func$2$f$',
+              sample: { id: 1 },
+              inputFields: [
+                {
+                  key: 'payload',
+                  type: 'json',
+                },
+              ],
+            },
+          },
+        },
+      };
+
+      const results = schema.validateAppDefinition(definition);
+      results.errors.should.have.length(0);
+    });
+
+    it('should error when schema is provided without type json', () => {
+      const definition = {
+        version: '1.0.0',
+        platformVersion: '1.0.0',
+        creates: {
+          foo: {
+            key: 'foo',
+            noun: 'Foo',
+            display: {
+              label: 'Create Foo',
+              description: 'Creates a...',
+            },
+            operation: {
+              perform: '$func$2$f$',
+              sample: { id: 1 },
+              inputFields: [
+                {
+                  key: 'payload',
+                  type: 'string',
+                  schema: { type: 'object' },
+                },
+              ],
+            },
+          },
+        },
+      };
+
+      const results = schema.validateAppDefinition(definition);
+      results.errors.should.have.length(1);
+      results.errors[0].stack.should.containEql(
+        'must have `type` set to `json` when `schema` is provided',
+      );
+    });
   });
 
-  it('should not error when type is json without schema', () => {
-    const definition = {
-      version: '1.0.0',
-      platformVersion: '1.0.0',
-      creates: {
-        foo: {
-          key: 'foo',
-          noun: 'Foo',
-          display: {
-            label: 'Create Foo',
-            description: 'Creates a...',
-          },
-          operation: {
-            perform: '$func$2$f$',
-            sample: { id: 1 },
-            inputFields: [
-              {
-                key: 'payload',
-                type: 'json',
+  describe('resource validation', () => {
+    it('should not error when type is json and schema is provided', () => {
+      const definition = {
+        version: '1.0.0',
+        platformVersion: '1.0.0',
+        resources: {
+          contact: {
+            key: 'contact',
+            noun: 'Contact',
+            create: {
+              display: {
+                label: 'Create Contact',
+                description: 'Create a new contact',
               },
-            ],
+              operation: {
+                perform: '$func$2$f$',
+                inputFields: [
+                  {
+                    key: 'payload',
+                    type: 'json',
+                    schema: {
+                      type: 'object',
+                      properties: {
+                        name: { type: 'string' },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
           },
         },
-      },
-    };
+      };
 
-    const results = schema.validateAppDefinition(definition);
-    results.errors.should.have.length(0);
-  });
+      const results = schema.validateAppDefinition(definition);
+      results.errors.should.have.length(0);
+    });
 
-  it('should error when schema is provided without type json', () => {
-    const definition = {
-      version: '1.0.0',
-      platformVersion: '1.0.0',
-      creates: {
-        foo: {
-          key: 'foo',
-          noun: 'Foo',
-          display: {
-            label: 'Create Foo',
-            description: 'Creates a...',
-          },
-          operation: {
-            perform: '$func$2$f$',
-            sample: { id: 1 },
-            inputFields: [
-              {
-                key: 'payload',
-                type: 'string',
-                schema: { type: 'object' },
+    it('should error when schema is provided without type json', () => {
+      const definition = {
+        version: '1.0.0',
+        platformVersion: '1.0.0',
+        resources: {
+          contact: {
+            key: 'contact',
+            noun: 'Contact',
+            create: {
+              display: {
+                label: 'Create Contact',
+                description: 'Create a new contact',
               },
-            ],
+              operation: {
+                perform: '$func$2$f$',
+                inputFields: [
+                  {
+                    key: 'payload',
+                    type: 'string',
+                    schema: { type: 'object' },
+                  },
+                ],
+              },
+            },
           },
         },
-      },
-    };
+      };
 
-    const results = schema.validateAppDefinition(definition);
-    results.errors.should.have.length(1);
-    results.errors[0].stack.should.containEql(
-      'must have `type` set to `json` when `schema` is provided',
-    );
-  });
-
-  it('should error when schema is provided with no type at all', () => {
-    const definition = {
-      version: '1.0.0',
-      platformVersion: '1.0.0',
-      creates: {
-        foo: {
-          key: 'foo',
-          noun: 'Foo',
-          display: {
-            label: 'Create Foo',
-            description: 'Creates a...',
-          },
-          operation: {
-            perform: '$func$2$f$',
-            sample: { id: 1 },
-            inputFields: [
-              {
-                key: 'payload',
-                schema: { type: 'object' },
-              },
-            ],
-          },
-        },
-      },
-    };
-
-    const results = schema.validateAppDefinition(definition);
-    results.errors.should.have.length(1);
-    results.errors[0].stack.should.containEql(
-      'must have `type` set to `json` when `schema` is provided',
-    );
+      const results = schema.validateAppDefinition(definition);
+      results.errors.should.have.length(1);
+      results.errors[0].stack.should.containEql(
+        'must have `type` set to `json` when `schema` is provided',
+      );
+    });
   });
 });
